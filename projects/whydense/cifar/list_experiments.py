58140f7d01143ca469899aa54d3370a309440f3c
Subutai Ahmad <sahmad@Subutai-Laptop-2019.local>
2019-04-25 14:55:10 -0700

    Fix list experiments to look at all experiments in path

--- projects/whydense/cifar/list_experiments.py
+++ projects/whydense/cifar/list_experiments.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
+import glob
 import tabulate
 import pprint
 import click
@@ -19,32 +21,69 @@ def print_df_tabular(df):
   print(tabulate(table, headers="firstrow", tablefmt="grid"))
 
 
-class experimentBrowser(object):
+def get_experiment_states(experiment_path, exit_on_fail=False):
+  """
+  Return every experiment state JSON file in the path as a list of dicts.
+  The list is sorted such that newer experiments appear later.
+  """
+  experiment_path = os.path.expanduser(experiment_path)
+  experiment_state_paths = glob.glob(
+    os.path.join(experiment_path, "experiment_state*.json"))
+  if not experiment_state_paths:
+    if exit_on_fail:
+      print("No experiment state found!")
+      sys.exit(0)
+    else:
+      return
+
+  experiment_state_paths = list(experiment_state_paths)
+  experiment_state_paths.sort()
+  experiment_states = []
+  for experiment_filename in list(experiment_state_paths):
+
+    with open(experiment_filename) as f:
+      experiment_states.append(json.load(f))
+
+  return experiment_states
+
+
+class ExperimentBrowser(object):
 
   def __init__(self, experiment_path):
     self.experiment_path = os.path.abspath(experiment_path)
-    self.experiment_state = _get_experiment_state(
+    self.experiment_states = get_experiment_states(
       self.experiment_path, exit_on_fail=True)
-    print("Examining experiments in ",
-          self.experiment_state["runner_data"]["_session_str"])
-    checkpoint_dicts = self.experiment_state["checkpoints"]
-    checkpoint_dicts = [flatten_dict(g) for g in checkpoint_dicts]
 
     self.progress = {}
     self.exp_directories = {}
     self.checkpoint_directories = {}
+    for experiment_state in self.experiment_states:
+      self._read_experiment(experiment_state)
+
+
+  def _read_experiment(self, experiment_state):
+    print("Examining experiments in ",
+          experiment_state["runner_data"]["_session_str"])
+    checkpoint_dicts = experiment_state["checkpoints"]
+    checkpoint_dicts = [flatten_dict(g) for g in checkpoint_dicts]
+
     for exp in checkpoint_dicts:
       exp_dir = os.path.basename(exp["logdir"])
-      csv = os.path.join(experiment_path, exp_dir, "progress.csv")
+      csv = os.path.join(self.experiment_path, exp_dir, "progress.csv")
       self.progress[exp["experiment_tag"]] = pd.read_csv(csv)
       self.exp_directories[exp["experiment_tag"]] = os.path.abspath(
-        os.path.join(experiment_path, exp_dir))
+        os.path.join(self.experiment_path, exp_dir))
 
-      # Figure out checkpoint file if it exists
-      cd = os.path.abspath(
-        os.path.join(experiment_path, exp_dir, "checkpoint_20"))
-      self.checkpoint_directories[exp["experiment_tag"]] = os.path.abspath(
-        os.path.join(experiment_path, exp_dir, "checkpoint_20", "model.pt"))
+      # Figure out checkpoint file if it exists. For some reason we need to
+      # switch to the directory in order for glob to work.
+      ed = os.path.abspath(os.path.join(self.experiment_path, exp_dir))
+      os.chdir(ed)
+      cd = max(glob.glob("checkpoint*"))
+      cf = glob.glob(os.path.join(cd, "*.pt"))
+      if len(cf) > 0:
+        self.checkpoint_directories[exp["experiment_tag"]] = os.path.abspath(cf[0])
+      else:
+        self.checkpoint_directories[exp["experiment_tag"]] = ""
 
 
   def get_value(self, exp_substring="", tag="mean_accuracy", which='max'):
@@ -101,7 +140,7 @@ class experimentBrowser(object):
 @click.option('--which', default="max", help='The function to use for extracting')
 def summarize_trials(experiment_path, name, tag, which):
     """Summarizes trials in the directory subtree starting at the given path."""
-    browser = experimentBrowser(experiment_path)
+    browser = ExperimentBrowser(experiment_path)
     p = browser.get_value(exp_substring=name, tag=tag, which=which)
     p.sort_values(tag, axis=0, ascending=False,
                  inplace=True, na_position='last')
