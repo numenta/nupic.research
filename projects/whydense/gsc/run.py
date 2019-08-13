acba87b7f3bf06651e2e7b7dcdd46231c59e3974
Subutai Ahmad <sahmad@Subutai-Laptop-2019.local>
2019-08-13 16:50:24 -0700

    Changed handling of paths

--- projects/whydense/gsc/run.py
+++ projects/whydense/gsc/run.py
@@ -21,6 +21,7 @@
 import json
 import os
 import subprocess
+from pathlib import Path
 
 import click
 import numpy as np  # noqa F401
@@ -154,11 +155,6 @@ def train(config, experiments, num_cpus, num_gpus, redis_address, show_list):
     print("num_cpus =", num_cpus)
     print("redis_address =", redis_address)
 
-    # Use configuration file location as the project location.
-    project_dir = os.path.dirname(config.name)
-    project_dir = os.path.abspath(project_dir)
-    print("project_dir =", project_dir)
-
     # Load and parse experiment configurations
     configs = parse_config(config, experiments, globals_param=globals())
 
@@ -186,14 +182,13 @@ def train(config, experiments, num_cpus, num_gpus, redis_address, show_list):
         stop_criteria.update(config.get("stop", {}))
         print("stop_criteria =", stop_criteria)
 
-        # Make sure local directories are relative to the project location
-        path = config.get("path", None)
-        if path and not os.path.isabs(path):
-            config["path"] = os.path.join(project_dir, path)
+        # Make sure path and data_dir are relative to the project location,
+        # handling both ~/nta and ../results style paths.
+        path = config.get("path", ".")
+        config["path"] = str(Path(path).expanduser().resolve())
 
         data_dir = config.get("data_dir", "data")
-        if not os.path.isabs(data_dir):
-            config["data_dir"] = os.path.join(project_dir, data_dir)
+        config["data_dir"] = str(Path(data_dir).expanduser().resolve())
 
         tune.run(
             SpeechExperimentTune,
@@ -261,11 +256,6 @@ def noise(config, experiments, num_cpus, num_gpus, redis_address):
     print("num_cpus =", num_cpus)
     print("redis_address =", redis_address)
 
-    # Use configuration file location as the project location.
-    project_dir = os.path.dirname(config.name)
-    project_dir = os.path.abspath(project_dir)
-    print("project_dir =", project_dir)
-
     # Load and parse experiment configurations
     configs = parse_config(config, experiments, globals_param=globals())
 
@@ -286,14 +276,13 @@ def noise(config, experiments, num_cpus, num_gpus, redis_address):
         config = configs[exp]
         config["name"] = exp
 
-        # Make sure local directories are relative to the project location
-        path = config.get("path", None)
-        if path and not os.path.isabs(path):
-            config["path"] = os.path.join(project_dir, path)
+        # Make sure path and data_dir are relative to the project location,
+        # handling both ~/nta and ../results style paths.
+        path = config.get("path", ".")
+        config["path"] = str(Path(path).expanduser().resolve())
 
         data_dir = config.get("data_dir", "data")
-        if not os.path.isabs(data_dir):
-            config["data_dir"] = os.path.join(project_dir, data_dir)
+        config["data_dir"] = str(Path(data_dir).expanduser().resolve())
 
         # Run each experiment in parallel
         results.append(run_noise_test.remote(config))
