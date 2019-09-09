918b855597ae54d0f5f6ffccf65f555d41d6274f
lucasosouza <lucasosouza@gmail.com>
2019-09-09 09:44:13 -0700

    removes E712 exception from setup and updates hebbian pruning tests

--- projects/dynamic_sparse/runs/improved_mag_gsc.py
+++ projects/dynamic_sparse/runs/improved_mag_gsc.py
@@ -18,3 +18,59 @@
 #
 # http://numenta.org/licenses/
 # ----------------------------------------------------------------------
+
+
+import os
+
+from ray import tune
+
+from nupic.research.frameworks.dynamic_sparse.common.loggers import DEFAULT_LOGGERS
+from nupic.research.frameworks.dynamic_sparse.common.utils import run_ray, run_ray_many
+
+# experiment configurations
+base_exp_config = dict(
+    device="cuda",
+    # dataset related
+    dataset_name="PreprocessedGSC",
+    data_dir=os.path.expanduser("~/nta/datasets/gsc"),
+    batch_size_train=(4, 16),
+    batch_size_test=1000,    
+    # network related
+    network="gsc_conv_heb",
+    optim_alg="SGD",
+    momentum=0,
+    learning_rate=0.01,
+    weight_decay=0.01,
+    lr_scheduler="MultiStepLR",
+    lr_milestones=[30,60,90],
+    lr_gamma=0.90,
+    use_kwinners=True,
+    # model related
+    model=tune.grid_search(["DSNNWeightedMag", "DSNNMixedHeb"]),
+    on_perc=tune.grid_search([0.1, 0.05, 0.04, 0.03]),
+    # sparse related
+    hebbian_prune_perc=None,
+    hebbian_grow=False,
+    weight_prune_perc=tune.grid_search([None, 0.2, 0.3, 0.4]),
+    pruning_early_stop=tune.grid_search([1, 2]),
+    # additional validation
+    test_noise=False,
+    # debugging
+    debug_weights=True,
+    debug_sparse=True,
+)
+
+# ray configurations
+tune_config = dict(
+    name=__file__.replace(".py", "") + "_eval1",
+    num_samples=3,
+    local_dir=os.path.expanduser("~/nta/results"),
+    checkpoint_freq=0,
+    checkpoint_at_end=False,
+    stop={"training_iteration": 100},
+    resources_per_trial={"cpu": 1, "gpu": 0.165},
+    loggers=DEFAULT_LOGGERS,
+    verbose=0,
+)
+
+run_ray(tune_config, base_exp_config)
