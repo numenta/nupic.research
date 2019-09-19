6b2dfcb2a99caa21691926a1b55b02d23f4d11e5
Michaelangelo <mvacaporale@gmail.com>
2019-09-19 11:34:03 -0700

    New functionality for previous commit (forgot to include).

--- nupic/research/frameworks/dynamic_sparse/models/main.py
+++ nupic/research/frameworks/dynamic_sparse/models/main.py
@@ -29,7 +29,6 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.optim.lr_scheduler as schedulers
 
-from nupic.research.frameworks.dynamic_sparse.networks import DynamicSparseBase
 from nupic.torch.modules import update_boost_strength
 
 
@@ -231,11 +230,7 @@ class SparseModel(BaseModel):
         self.num_params = []  # added for paper implementation
 
         # define sparse modules
-        self.sparse_modules = []
-        for m in list(self.network.modules()):
-            if isinstance(m, DynamicSparseBase):
-                # TODO: Could be more fitting to name this 'dynamic_sparse_modules'
-                self.sparse_modules.append(m)
+        self.sparse_modules = self.get_sparse_modules(self.network)
 
         # added option to define sparsity by on_perc
         if "on_perc" in self.__dict__:
@@ -257,6 +252,32 @@ class SparseModel(BaseModel):
                 m.weight.data *= mask
                 self.masks.append(mask)
                 self.num_params.append(torch.sum(mask).item())
+
+    @classmethod
+    def get_sparse_modules(cls, net):
+        """
+        This function recursively finds which modules to make sparse
+        and which to remain dense. The encapsulated logic crucially
+        assumes that no conv or linear layers have sub-children that need
+        sparsifying.
+
+        Note: For instance DSConv2d layers have Conv2d children)
+        """
+
+        sparse_modules = []
+        for m in net.children():
+
+            # Check if Conv or Linear.
+            if isinstance(m, (nn.Linear, nn.Conv2d)):
+                sparse_modules.append(m)
+
+            # Check if recursion needed.
+            elif len(list(m.children())) > 0:
+                sparse_modules.extend(
+                    cls.get_sparse_modules(m)
+                )
+
+        return sparse_modules
 
     def _make_attr_iterable(self, attr):
         """
