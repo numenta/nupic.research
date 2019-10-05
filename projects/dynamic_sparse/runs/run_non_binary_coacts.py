9e02664821d8a8c608367bed818347a2da8ab08b
Michaelangelo <mvacaporale@gmail.com>
2019-10-04 17:04:01 -0700

    Conform to PEP8 standards.

--- projects/dynamic_sparse/runs/run_non_binary_coacts.py
+++ projects/dynamic_sparse/runs/run_non_binary_coacts.py
@@ -21,10 +21,9 @@
 
 import os
 
+import numpy as np
 import ray
 import torch
-
-import numpy as np
 
 from nupic.research.frameworks.dynamic_sparse.common.loggers import DEFAULT_LOGGERS
 from nupic.research.frameworks.dynamic_sparse.common.utils import (
