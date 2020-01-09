89180098632078c0a00fa4e11db8e1b3d7277e9d
Subutai Ahmad <sahmad@Subutai-Laptop-2019.local>
2020-01-09 15:26:55 -0800

    Reconfigured experiments to add custom params
    Made hdf5 the default again

--- projects/imagenet/experiments.py
+++ projects/imagenet/experiments.py
@@ -1,5 +1,5 @@
 #  Numenta Platform for Intelligent Computing (NuPIC)
-#  Copyright (C) 2019, Numenta, Inc.  Unless you have an agreement
+#  Copyright (C) 2020, Numenta, Inc.  Unless you have an agreement
 #  with Numenta, Inc., for a separate license for this software code, the
 #  following terms and conditions appl":
 #
@@ -40,7 +40,7 @@ DEFAULT = dict(
     # Results path
     local_dir=os.path.expanduser("~/nta/results/imagenet"),
     # Dataset path
-    data=os.path.expanduser("~/nta/data/imagenet"),
+    data=os.path.expanduser("~/nta/data/imagenet/imagenet.hdf5"),
     # Dataset training data relative path
     train_dir="train",
     # Dataset validation data relative path
