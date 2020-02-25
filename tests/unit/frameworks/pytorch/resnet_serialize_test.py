c4209b27444ea2340adea922f249da66cff23b13
Ares Fisher <afisher@Ares-MacBook-Pro.local>
2020-02-25 15:16:39 -0800

    rewrote resnet50 serialization test to reflect workflow, RES-1287

--- tests/unit/frameworks/pytorch/resnet_serialize_test.py
+++ tests/unit/frameworks/pytorch/resnet_serialize_test.py
@@ -18,52 +18,62 @@
 #  http://numenta.org/licenses/
 #
 
-
+import io
+import pickle
 import tempfile
 import unittest
 
 import torch
 
+import nupic.research.frameworks.pytorch.models.resnets
 from nupic.research.frameworks.pytorch.imagenet.experiment_utils import create_model
 from nupic.research.frameworks.pytorch.model_compare import compare_models
-from nupic.research.frameworks.pytorch.models.resnets import resnet50
+from nupic.research.frameworks.pytorch.model_utils import serialize_state_dict
+
+
+def save_model(model):
+    state = {}
+    with io.BytesIO() as buffer:
+        serialize_state_dict(buffer, model.state_dict())
+        state["model"] = buffer.getvalue()
+
+    checkpoint_path = tempfile.NamedTemporaryFile(delete=True)
+    pickle.dump(state, checkpoint_path)
+    checkpoint_path.flush()
+    return checkpoint_path
 
 
 class ResNetSerialization(unittest.TestCase):
-    """Test if loaded model is identical to initially saved one"""
     def test_identical(self):
-        # model args for ResNet, may become function arguments later on
-        model_args = dict(config=dict(
-            num_classes=3,
-            defaults_sparse=True,
-        ))
-        model_class = resnet50
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-        model = create_model(
-            model_class=model_class,
-            model_args=model_args,
-            init_batch_norm=False,
-            device=device,
-        )
-        # create temp file
-        model_path = tempfile.NamedTemporaryFile(delete=True)
-        # and write the created model
-        torch.save(model.state_dict(), model_path)
-
-        model_copy = create_model(
-            model_class=model_class,
-            model_args=model_args,
-            init_batch_norm=False,
-            device=device,
-        )
-        # copy the initial model parameters to the new model
-        model_copy.load_state_dict(torch.load(model_path.name))
-
-        # comapre the initial and new model
-        self.assertTrue(compare_models(model, model_copy, input_shape=(3, 224, 224)))
-        model_path.close()
+        self.assertTrue(compare_models(model, model2, (3, 224, 224)))
 
 
 if __name__ == "__main__":
+    model_args = dict(config=dict(
+        num_classes=3,
+        defaults_sparse=True,
+    ))
+    model_class = nupic.research.frameworks.pytorch.models.resnets.resnet50
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model = create_model(
+        model_class=model_class,
+        model_args=model_args,
+        init_batch_norm=False,
+        device=device,
+    )
+
+    checkpoint_file = save_model(model)
+
+    model2 = create_model(
+        model_class=model_class,
+        model_args=model_args,
+        init_batch_norm=False,
+        device=device,
+        checkpoint_file=checkpoint_file.name
+    )
+
+    checkpoint_file.close()
+    assert checkpoint_file.file.closed
+    print("Comparing: ")
     unittest.main(verbosity=2)
