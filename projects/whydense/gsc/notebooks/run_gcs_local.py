ffc9741768490d37b2a9e40275bf6f235a7e4e5f
Ares Fisher <afisher@Ares-MacBook-Pro.local>
2020-02-27 17:18:26 -0800

    minor, do not merge

--- projects/whydense/gsc/notebooks/run_gcs_local.py
+++ projects/whydense/gsc/notebooks/run_gcs_local.py
@@ -4,7 +4,7 @@ sys.path.append('../')
 from sparse_speech_experiment import *
 import configparser
 from nupic.research.support import parse_config
-
+import numpy as np
 
 config_file = "../experiments.cfg"
 exp = "sparseCNN2"
@@ -16,10 +16,15 @@ config["data_dir"] = "/Users/afisher/nta/nupic.torch/examples/gsc/data/"
 
 experiment = SparseSpeechExperiment(config=config)
 
-n_epochs = 10
+n_epochs = 4
 
 for epoch in range(n_epochs):
+    start_time = time.time()
+    print("Epoch: {}".format(epoch))
+
     experiment.train(epoch)
-    experiment.validate()
-    
-experiment.test()
+    end_time = time.time()
+    print(experiment.validate())
+    print("in {} s".format(np.round(end_time - start_time, 3)))
+
+print("Test: {}".format(experiment.test()))
