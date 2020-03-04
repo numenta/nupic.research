e6f0f557dc4cf4f2d6327489cc65230f940b7b95
Michaelangelo <mvacaporale@gmail.com>
2020-03-03 16:35:24 -0800

    Fix util import.

--- nupic/research/frameworks/dynamic_sparse/common/ray_custom_loggers.py
+++ nupic/research/frameworks/dynamic_sparse/common/ray_custom_loggers.py
@@ -34,9 +34,11 @@ import PIL.Image
 import seaborn as sns
 from ray.tune.logger import CSVLogger, JsonLogger, Logger
 from ray.tune.result import TIME_TOTAL_S, TIMESTEPS_TOTAL, TRAINING_ITERATION
-from ray.tune.utils import flatten_dict
+from ray.tune.util import flatten_dict
 
 logger = logging.getLogger(__name__)
+
+# import tensorflow as tf  # noqa: @ remove
 
 
 def to_tf_values(result, path, histo_bins=1000):
