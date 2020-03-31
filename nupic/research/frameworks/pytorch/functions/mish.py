3cd5fef54418998dc580bb1fd28c90bcd7b32202
Michaelangelo <mvacaporale@gmail.com>
2020-03-31 16:05:31 -0700

    Mish activation
    Introduce Mish activation function and module.

---
+++ nupic/research/frameworks/pytorch/functions/mish.py
@@ -0,0 +1,34 @@
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2020, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
+import torch
+import torch.nn.functional as F
+
+
+def mish(x):
+    """
+    Applies the mish function element-wise:
+    mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))
+
+    Code adapted from:
+       https://github.com/digantamisra98/Mish/blob/master/Mish/Torch/functional.py
+    """
+    return x * torch.tanh(F.softplus(x))
