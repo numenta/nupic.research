502015412165f41f1b03330ea8b07a35375190ba
Ares Fisher <afisher@Ares-MBP.lan>
2020-05-27 15:45:54 -0700

    Revert "more flake8"
    This reverts commit 375b4480fa91292db44e7de266138654fd170fe2.

--- nupic/research/support/parse_config.py
+++ nupic/research/support/parse_config.py
@@ -33,7 +33,7 @@ def parse_config(config_file, experiments=None, globals_param=None, locals_param
     """
     cfgparser = configparser.ConfigParser()
     cfgparser.read_file(config_file)
-
+    
     params = {}
     for exp in cfgparser.sections():
         if not experiments or exp in experiments:
