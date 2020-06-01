d9782c3e989543f4f9fc7b90495b0e86497d3391
Lucas <lucasosouza@gmail.com>
2020-06-01 13:17:01 -0700

    ündo changes to dataloader creation

--- nupic/research/frameworks/pytorch/imagenet/imagenet_experiment.py
+++ nupic/research/frameworks/pytorch/imagenet/imagenet_experiment.py
@@ -367,8 +367,7 @@ class ImagenetExperiment:
             shuffle=False,
             num_workers=config.get("workers", 0),
             sampler=sampler,
-            pin_memory=False,
-            # pin_memory=torch.cuda.is_available(),
+            pin_memory=torch.cuda.is_available(),
         )
 
     def validate(self, loader=None):
