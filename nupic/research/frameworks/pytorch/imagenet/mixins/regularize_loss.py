4f08e908fe9d54883c0c672b8585f3f52d8ae3b3
Marcus Lewis <mrcslws@gmail.com>
2020-06-04 09:06:17 -0700

    RegularizeLoss: Batch-based schedule. Use torch.stack.

--- nupic/research/frameworks/pytorch/imagenet/mixins/regularize_loss.py
+++ nupic/research/frameworks/pytorch/imagenet/mixins/regularize_loss.py
@@ -19,6 +19,8 @@
 # http://numenta.org/licenses/
 # ----------------------------------------------------------------------
 
+import torch
+
 
 class RegularizeLoss(object):
     """
@@ -27,62 +29,74 @@ class RegularizeLoss(object):
     """
     def setup_experiment(self, config):
         """
-        @param reg_schedule (list of tuples)
-        Mapping from epoch number to the reg_weight to use on that timestep and
-        afterward.
-
-        @param downscale_reg_with_training_set (bool)
-        If True, multiply the regularization term by (1 / size_of_training_set)
+        :param config:
+            Dictionary containing the following parameters that set the schedule
+            for the regularization strength:
+            - initial_reg_scalar, final_reg_scalar
+            - pct_begin_reg_ramp, pct_end_reg_ramp
+            - num_reg_cycles: How many times to repeat the reg schedule
+            - downscale_reg_with_training_set: If True, multiply the
+                 regularization term by (1 / size_of_training_set)
         """
         super().setup_experiment(config)
 
-        if config.get("downscale_reg_with_training_set", False):
-            self.reg_coefficient = (
-                1 / len(self.train_loader.dataset)
-            )
-        else:
-            self.reg_coefficient = 1
+        self.num_reg_cycles = config.get("num_reg_cycles", 1)
+        self.initial_reg_scalar = config.get("initial_reg_scalar", 1.0)
+        self.final_reg_scalar = config.get("final_reg_scalar", 1.0)
+        self.pct_begin_ramp = config.get("pct_begin_reg_ramp", 0.0)
+        self.pct_end_ramp = config.get("pct_end_reg_ramp", 0.0)
+        self.reg_coefficient = (
+            1 / len(self.train_loader.dataset)
+            if config.get("downscale_reg_with_training_set", False)
+            else 1
+        )
 
-        reg_schedule = config.get("reg_schedule", None)
-        if reg_schedule is None:
-            self.reg_schedule = {}
-            self.reg_weight = 1.0
-        else:
-            self.reg_schedule = dict(reg_schedule)
-            self.reg_weight = self.reg_schedule[0]
+        # Cache these floats
+        self.reg_ramp_window_length = self.pct_end_ramp - self.pct_begin_ramp
+        self.reg_ramp_amount = self.final_reg_scalar - self.initial_reg_scalar
+
+        self.reg_scalar = self._compute_reg_scalar(0.0)
 
         self._regularized_modules = [module
                                      for module in self.model.modules()
                                      if hasattr(module, "regularization")]
+        assert len(self._regularized_modules) > 0
+
+    def _compute_reg_scalar(self, pct):
+        if pct < self.pct_begin_ramp:
+            return self.initial_reg_scalar
+        elif pct < self.pct_end_ramp:
+            ramp_pct = (pct - self.pct_begin_ramp) / self.reg_ramp_window_length
+            return (self.initial_reg_scalar
+                    + ramp_pct * self.reg_ramp_amount)
+        else:
+            return self.final_reg_scalar
+
+    def pre_batch(self, model, batch_idx):
+        super().pre_batch(model, batch_idx)
+
+        epochs_per_cycle = self.epochs / self.num_reg_cycles
+        epoch = self.current_epoch % epochs_per_cycle
+
+        pct = ((epoch * self.total_batches + batch_idx)
+               / (epochs_per_cycle * self.total_batches))
+        self.reg_scalar = self._compute_reg_scalar(pct)
 
     def complexity_loss(self, model):
         c_loss = super().complexity_loss(model)
-
-        if self.reg_weight != 0 and len(self._regularized_modules) > 0:
-            # This is inner loop code, avoid any unnecessary tensor allocation.
-            reg = None
-            for module in self._regularized_modules:
-                if reg is None:
-                    reg = module.regularization()
-                else:
-                    reg += module.regularization()
-
-            reg *= self.reg_weight * self.reg_coefficient
-            c_loss = (c_loss + reg
-                      if c_loss is not None
-                      else reg)
-
+        assert model is self.model
+        reg = torch.stack([module.regularization()
+                           for module in self._regularized_modules]).sum()
+        reg *= self.reg_scalar * self.reg_coefficient
+        c_loss = (c_loss + reg
+                  if c_loss is not None
+                  else reg)
         return c_loss
-
-    def pre_epoch(self):
-        super().pre_epoch()
-        if self.current_epoch in self.reg_schedule:
-            self.reg_weight = self.reg_schedule[self.current_epoch]
 
     @classmethod
     def get_execution_order(cls):
         eo = super().get_execution_order()
-        eo["setup_experiment"].append("RegularizeLoss initialization")
-        eo["complexity_loss"].append("RegularizeLoss")
-        eo["pre_epoch"].append("RegularizeLoss update regularization weight")
+        eo["setup_experiment"].append("RegularizeLoss: initialization")
+        eo["complexity_loss"].append("RegularizeLoss: Compute")
+        eo["pre_batch"].append("RegularizeLoss: update regularization scalar")
         return eo
