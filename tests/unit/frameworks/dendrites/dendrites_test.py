170b8ccb3bde5cdb26eb0e93fe96c0df4b7d727a
Michaelangelo <mvacaporale@gmail.com>
2020-09-29 13:03:08 -0400

    Add and enhance tests.

--- tests/unit/frameworks/dendrites/dendrites_test.py
+++ tests/unit/frameworks/dendrites/dendrites_test.py
@@ -33,6 +33,8 @@ from nupic.research.frameworks.dendrites import (
 class DendriteSegmentsTests(unittest.TestCase):
 
     def test_forward(self):
+        """Validate shape of forward output."""
+
         dendrite_segments = DendriteSegments(
             num_units=10, num_segments=20, dim_context=15, sparsity=0.7, bias=True
         )
@@ -43,10 +45,60 @@ class DendriteSegmentsTests(unittest.TestCase):
         out = dendrite_segments(context)
         self.assertEqual(out.shape, (8, 10, 20))
 
+    def test_sparsity(self):
+        """
+        Validate that sparsity is enforced per unit per segement.
+        """
+        sparsity = 9 / 15
+        dendrite_segments = DendriteSegments(
+            num_units=10, num_segments=20, dim_context=15, sparsity=sparsity, bias=True
+        )
+
+        weights = dendrite_segments.weights
+        weights[:] = 1
+        dendrite_segments.rezero_weights()
+
+        for unit in range(dendrite_segments.num_units):
+            for segment in range(dendrite_segments.num_segments):
+                w = weights[unit, segment, :]
+                num_off = (weights[unit, segment, :] == 0).sum().item()
+                actual_sparsity = num_off / w.numel()
+                self.assertEqual(sparsity, actual_sparsity,
+                                 f"Sparsity {actual_sparsity} != {sparsity}"
+                                 f"for unit {unit} and segment {segment}")
+
+    def test_equivalent_forward(self):
+        """
+        Validate output with respect to an equivalent operation:
+        applying the dendrite segments one-by-one for each unit.
+        """
+        dendrite_segments = DendriteSegments(
+            num_units=10, num_segments=20, dim_context=15, sparsity=0.7, bias=True
+        )
+
+        batch_size = 8
+        context = torch.rand(batch_size, dendrite_segments.dim_context)
+        out = dendrite_segments(context)  # shape batch_size x num_units x num_segments
+
+        weights = dendrite_segments.weights
+        biases = dendrite_segments.biases
+        for unit in range(dendrite_segments.num_units):
+            unit_weight = weights[unit, ...]
+            unit_bias = biases[unit, ...]
+
+            expected_out = torch.nn.functional.linear(context, unit_weight, unit_bias)
+            actual_out = out[:, unit, :]
+            same_out = torch.allclose(actual_out, expected_out, atol=1e-7)
+            self.assertTrue(same_out,
+                            f"Didn't observe the expected output for unit {unit}: "
+                            f"actual_out - expected_out = {actual_out - expected_out}")
+
 
 class DendriticWeightsTests(unittest.TestCase):
 
     def test_forward(self):
+        """Validate shape of forward output."""
+
         # Dendritic weights as a bias.
         linear = torch.nn.Linear(10, 10)
         dendritic_weights = DendriticWeights(
@@ -64,10 +116,39 @@ class DendriticWeightsTests(unittest.TestCase):
         out = dendritic_weights(x, context)
         self.assertEqual(out.shape, (8, 10))
 
-    def test_gating_forward(self):
-        # Gating dendritic weights.
+    def test_sparsity(self):
+        """
+        Ensure both the linear weights and segment weights are rezeroed properly.
+        """
+        linear_sparsity = 70 / 100
+        dendrite_sparsity = 13 / 15
         linear = torch.nn.Linear(10, 10)
         dendritic_weights = DendriticWeights(
+            module=linear, num_segments=20, dim_context=15,
+            module_sparsity=linear_sparsity, dendrite_sparsity=dendrite_sparsity,
+            dendrite_bias=True
+        )
+
+        linear_weights = linear.weight.data
+        dendrite_weights = dendritic_weights.segments.weights
+        linear_weights[:] = 1
+        dendrite_weights[:] = 1
+        dendritic_weights.rezero_weights()
+
+        num_off = (dendrite_weights == 0).sum().item()
+        actual_dendrite_sparsity = num_off / dendrite_weights.numel()
+        self.assertEqual(dendrite_sparsity, actual_dendrite_sparsity)
+
+        num_off = (linear_weights == 0).sum().item()
+        actual_linear_sparsity = num_off / linear_weights.numel()
+        self.assertEqual(linear_sparsity, actual_linear_sparsity)
+
+    def test_gating_forward(self):
+        """Validate shape of forward output."""
+
+        # Gating dendritic weights.
+        linear = torch.nn.Linear(10, 10)
+        dendritic_weights = GatingDendriticWeights(
             module=linear, num_segments=20, dim_context=15,
             module_sparsity=0.7, dendrite_sparsity=0.9, dendrite_bias=True
         )
