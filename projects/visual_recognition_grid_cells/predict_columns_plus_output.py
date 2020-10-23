b1097006044bef8873c8acaab6d586904dd5c55d
nielsleadholm <n.leadholm@gmail.com>
2020-10-23 14:27:42 +0100

    Add touch-based predictions, and visualisation of current touch

--- projects/visual_recognition_grid_cells/predict_columns_plus_output.py
+++ projects/visual_recognition_grid_cells/predict_columns_plus_output.py
@@ -43,118 +43,127 @@ DATA_SET = 'mnist'
 def predict_column_plus_reps(net, prediction_sequence, touch_sequence, label, prediction_validity, ground_truth):
 
     print("\n New prediction")
+    plt.imsave('predicted_images/' + label + '_ground_truth.png', ground_truth)
 
-    for width_iter in range(5):
-        for height_iter in range(5):
+    # for width_iter in range(5):
+    #     for height_iter in range(5):
+    for touch_iter in range(25):
 
-            touch_iter = 0
-            # touch_iter = width_iter*5 + height_iter
-            # print("Touch iter")
-            # print(touch_iter)
+        # touch_iter = width_iter*5 + height_iter
+        # print("Touch iter")
+        # print(touch_iter)
 
-            # if touch_iter >= len(touch_sequence):
-            #     # *** note touch sequence does not actually track how many touches were performed
-            #     print("Completed all touches")
-            #     break
+        # if touch_iter >= len(touch_sequence):
+        #     # *** note touch sequence does not actually track how many touches were performed
+        #     print("Completed all touches")
+        #     break
 
-            # touch_location = touch_sequence[touch_iter]
-            # print("Touch location and sequence")
-            # print(touch_location)
-            # print(touch_sequence)
+        # touch_location = touch_sequence[touch_iter]
+        # print("Touch location and sequence")
+        # print(touch_location)
+        # print(touch_sequence)
 
-            input_SDR = np.zeros([128, 5*5])
-            print("Initial SDR")
-            print(np.shape(input_SDR))
-            print(input_SDR)
+        input_SDR = np.zeros([128, 5*5])
+        # print("Initial SDR")
+        # print(np.shape(input_SDR))
+        # print(input_SDR)
 
-            # touch_indices = touch_sequence[0:touch_iter+1]
-            # # need to convert these indices to those of the expected SDR, may be easier to flattten it first **
-            # print("Touch indices")
-            # print(touch_indices)
-            # print(touch_indices[0])
+        # touch_indices = touch_sequence[0:touch_iter+1]
+        # # need to convert these indices to those of the expected SDR, may be easier to flattten it first **
+        # print("Touch indices")
+        # print(touch_indices)
+        # print(touch_indices[0])
 
-            print("Prediction SDR indices")
-            print(np.shape(prediction_sequence))
-            print("\n\n")
-            #print(prediction_sequence)
-            print("\n\n")
-            current_sequence = prediction_sequence[24]
-            print(prediction_sequence[24])
+        # print("Prediction SDR indices")
+        # print(np.shape(prediction_sequence))
+        # print("\n\n")
+        # #print(prediction_sequence)
+        # print("\n\n")
+        current_sequence = prediction_sequence[touch_iter]
+        # print(prediction_sequence[touch_iter])
 
-            print("Current sequence")
-            print(len(current_sequence))
-            print(current_sequence)
-            exit()
-
-
-            for sequence_iter in range(len(current_sequence)):
-                if len(current_sequence[sequence_iter]) > 0:
-                    print("Touch location")
-                    print(touch_sequence[sequence_iter])
-                    print("SDR indices")
-                    print(current_sequence[sequence_iter])
-                    print("SDR before modification")
-                    print(input_SDR[:, touch_sequence[sequence_iter]])
-                    input_SDR[current_sequence[sequence_iter], touch_sequence[sequence_iter-1]] = 1
-                    print("SDR after modification")
-                    print(input_SDR[:, touch_sequence[sequence_iter]])
-                else:
-                    print("\nOn iter " + str(sequence_iter) + " of the sequence, there is no sensation or prediction to use")
+        # print("Current sequence")
+        # print(len(current_sequence))
+        # print(current_sequence)
+        # exit()
 
 
-            print("Predicted SDR")
-            print(input_SDR)
-            print(np.shape(input_SDR))
-            input_SDR = torch.from_numpy(np.reshape(input_SDR, 128*5*5))
-            input_SDR = input_SDR.type(torch.DoubleTensor)
-            print(np.shape(input_SDR))
-            # print(input_SDR.dtype)
-
-            print("Reconstructed")
-            reconstructed = net(input_SDR)
-            # print(np.shape(reconstructed))
-
-            # *** highlight predicted location - note that as going from a 5*5 space to a 28*28 space, this is only approximate
-            highlight_width_lower, highlight_width_upper = (1 + width_iter*5),  (1 + (width_iter+1)*5)
-            highlight_height_lower, highlight_height_upper = (1 + height_iter*5),  (1 + (height_iter+1)*5)
-
-            highlight_array = np.zeros((28,28))
-            highlight_array[highlight_width_lower:highlight_width_upper, 
-                highlight_height_lower:highlight_height_upper] = 0.5
-
-            # print("Highlight_array")
-            # print(np.shape(highlight_array))
-            # print(highlight_array)
-
-            reconstructed = np.clip(reconstructed.detach().numpy() + highlight_array, 0, 1)
-
-
-            # *** #random comparison - change the current 
-            # print("Random control")
-            # print(touch_sequence[touch_iter])
-            # # random SDR (should have same sparsity as original SDRs)
-
-            # controlSDR = input_SDR[touch_sequence[touch_iter]][np.nonzero(np.random())]
-            # control_reconstructed = net(controlSDR)
-            if prediction_validity == True:
-                prediction = "correctly_classified"
+        for sequence_iter in range(len(current_sequence)):
+            if len(current_sequence[sequence_iter]) > 0:
+                print("\nOn iter " + str(sequence_iter))
+                print("Touch location")
+                print(touch_sequence[sequence_iter])
+                print("SDR indices")
+                print(current_sequence[sequence_iter])
+                print("SDR before modification")
+                print(input_SDR[:, touch_sequence[sequence_iter]])
+                input_SDR[current_sequence[sequence_iter], touch_sequence[sequence_iter-1]] = 1
+                print("SDR after modification")
+                print(input_SDR[:, touch_sequence[sequence_iter]])
             else:
-                prediction = "misclassified"
+                print("\nOn iter " + str(sequence_iter) + " of the sequence, there is no sensation or prediction to use")
 
-            # print(np.shape(reconstructed[0,:,:]))
-            # print(np.shape(ground_truth))
 
-            plt.imsave('predicted_images/' + label + '_ground_truth.png', ground_truth)
+        # print("Predicted SDR")
+        # print(input_SDR)
+        # print(np.shape(input_SDR))
+        input_SDR = torch.from_numpy(np.reshape(input_SDR, 128*5*5))
+        input_SDR = input_SDR.type(torch.DoubleTensor)
+        # print(np.shape(input_SDR))
+        # print(input_SDR.dtype)
+
+        print("\nReconstructing:")
+        reconstructed = net(input_SDR)
+        # print(np.shape(reconstructed))
+
+        # *** highlight predicted location - note that as going from a 5*5 space to a 28*28 space, this is only approximate
+        current_touch = touch_sequence[touch_iter]
+        print("Current touch")
+        print(current_touch)
+        print("Wdith and height iter")
+        width_iter = current_touch//5
+        height_iter = current_touch%5
+        highlight_width_lower, highlight_width_upper = (1 + width_iter*5),  (1 + (width_iter+1)*5)
+        highlight_height_lower, highlight_height_upper = (1 + height_iter*5),  (1 + (height_iter+1)*5)
+
+        highlight_array = np.zeros((28,28))
+        highlight_array[highlight_width_lower:highlight_width_upper, 
+            highlight_height_lower:highlight_height_upper] = 0.5
+
+        # print("Highlight_array")
+        # print(np.shape(highlight_array))
+        # print(highlight_array)
+
+        reconstructed = np.clip(reconstructed.detach().numpy() + highlight_array, 0, 1)
+
+
+        # *** #random comparison - change the current 
+        # print("Random control")
+        # print(touch_sequence[touch_iter])
+        # # random SDR (should have same sparsity as original SDRs)
+
+        # controlSDR = input_SDR[touch_sequence[touch_iter]][np.nonzero(np.random())]
+        # control_reconstructed = net(controlSDR)
+        if prediction_validity == True:
+            prediction = "correctly_classified"
+        else:
+            prediction = "misclassified"
+
+        # print(np.shape(reconstructed[0,:,:]))
+        # print(np.shape(ground_truth))
+
+        plt.imsave('predicted_images/' + label + '_' + prediction + 
+            '_touch_' + str(touch_iter) + '.png', reconstructed[0,:,:])
+
+        if touch_iter == 24:
+            #Save the final prediction without the prediction window 
             plt.imsave('predicted_images/' + label + '_' + prediction + 
                 '_touch_' + str(touch_iter) + '.png', reconstructed[0,:,:])
 
 
-            # plt.imsave('predicted_images/' + label + '_RandomControl_touch_' + str(touch_iter) + '.png')        
+        # plt.imsave('predicted_images/' + label + '_RandomControl_touch_' + str(touch_iter) + '.png')        
 
-            touch_iter += 1
 
-            break
-        break
 
 
 
@@ -185,4 +194,3 @@ if __name__ == '__main__':
             touch_sequence=current_object['touch_sequence'], label=current_object['name'], 
             prediction_validity=current_object['correctly_classified'],
             ground_truth=current_object['ground_truth_image'])
-
