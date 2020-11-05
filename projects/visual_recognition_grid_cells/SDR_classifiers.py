c90e99ff7dbcb96f501a6b6e0f351ea7ba702894
nielsleadholm <n.leadholm@gmail.com>
2020-11-05 20:59:50 +0000

    Add RNN for SDR classification

--- projects/visual_recognition_grid_cells/SDR_classifiers.py
+++ projects/visual_recognition_grid_cells/SDR_classifiers.py
@@ -28,8 +28,10 @@ import torch.nn.functional as F
 
 from sklearn.neighbors import KNeighborsClassifier
 
-data_set = 'fashion_mnist'
-shuffle_SDRs_bool = True
+data_set = 'mnist'
+num_samples_per_class = 5
+knn_bool = True
+n_neighbors = 1
 
 class MLP(torch.nn.Module):
     def __init__(self):
@@ -61,57 +63,144 @@ class linear(torch.nn.Module):
         x = self.fc(x)
         return x
 
-def train_net():
+class RNN_model(torch.nn.Module):
+    def __init__(self):
+        super(RNN_model, self).__init__()
 
-    net = MLP()
+        self.hidden_size = 64
+        self.num_layers = 1
+        self.rnn = torch.nn.RNN(input_size=128, hidden_size=self.hidden_size, num_layers=self.num_layers,
+            batch_first=True)
+        self.fc = torch.nn.Linear(self.hidden_size, 10)
 
-    training_data = torch.from_numpy(np.load(data_set + '_SDRs_training.npy'))
-    training_labels = torch.from_numpy(np.load(data_set + '_labels_training.npy'))
+        print("Using an RNN classifier")
 
-    # if shuffle_SDRs_bool == True:
-    #     for input_iter in len(training_labels:
-    #         training_data[input_iter, :, :]
+    def forward(self, x):
 
-    limited_samples = 50
-    print("\nOnly providing " + str(limited_samples) + " for training!")
-    training_data = training_data[0:limited_samples]
-    training_labels = training_labels[0:limited_samples]
-    print(np.shape(training_labels))
-    print(np.shape(training_data))
+        # print("X shapes")
+        # print(np.shape(x))
+
+        hidden = torch.zeros(self.num_layers, np.shape(x)[0], self.hidden_size)
+
+        x = x.reshape(-1, 4*4, 128)
+
+        # print(np.shape(x)) 
+
+        out, hidden = self.rnn(x, hidden)
+
+        # print("Out shapes")
+        # print(np.shape(out))
+
+        out = out[:, -1, :]
+
+        # print(np.shape(out))
+
+        # out = out.contiguous().view(-1, self.hidden_size)
+
+        # print(np.shape(out))
+        
+        out = self.fc(out)
+
+        # print(np.shape(out))
+
+        return out
 
 
-    testing_data = torch.from_numpy(np.load(data_set + '_SDRs_testing.npy'))
-    testing_labels = torch.from_numpy(np.load(data_set + '_labels_testing.npy'))
+
+def sub_sample_classes(input_data, labels, num_samples_per_class, sanity_check=None):
+
+    input_data_samples = []
+    label_samples = []
+
+    print("\n Loading " + str(num_samples_per_class) + " examples per class")
+
+    if sanity_check == 'one_class_training':
+        print("\nAs a sanity check, loading data for only a single class")
+        num_classes = 1
+    else:
+        num_classes = 10
+
+    for class_iter in range(num_classes):
+        indices = np.nonzero(labels == class_iter)
+
+        input_data_samples.extend(input_data[indices][0:num_samples_per_class])
+        label_samples.extend(labels[indices][0:num_samples_per_class])
+
+    print(np.shape(input_data_samples))
+    print(np.shape(label_samples))
+
+    return input_data_samples, label_samples
+
+def load_data(data_section='training', num_samples_per_class=5, sanity_check=None):
+
+    input_data = np.load(data_set + '_SDRs_' + data_section + '.npy')
+    labels = np.load(data_set + '_labels_' + data_section + '.npy')
+
+
+    input_data_samples, label_samples = sub_sample_classes(input_data, labels, num_samples_per_class, sanity_check=None)
+
+    return input_data_samples, label_samples
+
+def kNN(n_neighbors, training_data, training_labels, testing_data, testing_labels):
+
+    knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+
+    knn.fit(np.reshape(training_data, (np.shape(training_data)[0], np.shape(training_data)[1]*np.shape(training_data)[2]*np.shape(training_data)[3])), training_labels)
+
+    print("Accuracy of k-NN classifier " + str(knn.score(np.reshape(testing_data, (np.shape(testing_data)[0], np.shape(testing_data)[1]*np.shape(testing_data)[2]*np.shape(testing_data)[3])), testing_labels)))
 
 
 
-    knn = KNeighborsClassifier(n_neighbors=1)
+def train_net(net, training_data, training_labels, testing_data, testing_labels):
 
-    knn.fit(training_data, training_labels)
+    # input_data = torch.from_numpy(np.load(data_set + '_SDRs_' + data_section + '.npy'))
+    # labels = torch.from_numpy(np.load(data_set + '_labels_' + data_section + '.npy'))
 
-    print("Accuracy of knn " + str(knn.score(testing_data, testing_labels)))
+    training_data, training_labels, testing_data, testing_labels = (torch.FloatTensor(training_data), torch.LongTensor(training_labels),
+        torch.FloatTensor(testing_data), torch.LongTensor(testing_labels))
 
-    # criterion = torch.nn.CrossEntropyLoss()
-    # optimizer = torch.optim.SGD(net.parameters(), lr=1.0, momentum=0.9)
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-    # for epoch in range(1):  # loop over the dataset multiple times
+    for epoch in range(10000):  # loop over the dataset multiple times
 
-    #     optimizer.zero_grad()
+        optimizer.zero_grad()
 
-    #     outputs = net(training_data)
-    #     loss = criterion(outputs, training_labels)
-    #     loss.backward()
-    #     optimizer.step()
+        outputs = net(training_data)
+        loss = criterion(outputs, training_labels)
+        loss.backward()
+        optimizer.step()
 
-    #     training_acc = 100*(torch.sum(torch.argmax(outputs, dim=1)==training_labels)).item()/len(training_labels)
-    #     print("\nEpoch:" + str(epoch))
-    #     print("Training accuracy is " + str(training_acc))
+        training_acc = 100*(torch.sum(torch.argmax(outputs, dim=1)==training_labels)).item()/len(training_labels)
+        print("\nEpoch:" + str(epoch))
+        print("Training accuracy is " + str(training_acc))
 
-    #     testing_acc = 100*(torch.sum(torch.argmax(net(testing_data), dim=1)==testing_labels)).item()/len(testing_labels)
-    #     print("Testing accuracy is " + str(testing_acc))
+        testing_acc = 100*(torch.sum(torch.argmax(net(testing_data), dim=1)==testing_labels)).item()/len(testing_labels)
+        print("Testing accuracy is " + str(testing_acc))
 
-    # print('Finished Training')
+    print('Finished Training')
 
 if __name__ == '__main__':
-    train_net()
+
+    training_data, training_labels = load_data(data_section='training', num_samples_per_class=num_samples_per_class, sanity_check=None)
+
+    print(np.shape(training_data))
+    print(np.shape(training_labels))
+
+
+    testing_data, testing_labels = load_data(data_section='testing', num_samples_per_class=10, sanity_check=None)
+
+    print(np.shape(testing_data))
+    print(np.shape(testing_labels))
+
+
+    if knn_bool == True:
+
+        kNN(n_neighbors, training_data, training_labels, testing_data, testing_labels)
+
+    else:
+
+        net = RNN_model()
+
+        train_net(net, training_data, training_labels, testing_data, testing_labels)
     
