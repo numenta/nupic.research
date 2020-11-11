f04c8c31c9d1c348da984a4a6e4fca73a2c1b6bc
Michaelangelo <mvacaporale@gmail.com>
2020-11-11 15:41:09 -0500

    Move `metacl_oml_replicate` config.

--- projects/meta_cl/experiments/oml_replicate.py
+++ projects/meta_cl/experiments/oml_replicate.py
@@ -23,7 +23,7 @@ from copy import deepcopy
 
 from torchvision import transforms
 
-from .oml import metacl_oml_replicate
+from .oml import metacl_oml
 
 # """
 # These experiments serves to further narrow the gap between our implementation
@@ -55,19 +55,6 @@ transform_without_normalization = transforms.Compose([
 # --------------------
 
 
-# metacl_oml_replicate results: (re-tabulated here for reference)
-# |--------------------------------------------------------------|
-# |   Num Classes | Meta-test test   | Meta-test train   |  LR   |
-# |--------------:|:-----------------|:------------------|------:|
-# |            10 | 0.87 ± 0.07      | 0.95 ± 0.03       | 0.001 |
-# |            50 | 0.87 ± 0.02      | 0.96 ± 0.02       | 0.001 |
-# |           100 | 0.81 ± 0.02      | 0.94 ± 0.01       | 0.001 |
-# |           200 | 0.73 ± 0.02      | 0.91 ± 0.02       | 0.001 |
-# |           600 | 0.59 ± 0.01      | 0.85 ± 0.01       | 0.001 |
-# |--------------------------------------------------------------|
-#
-
-
 meta_test_test_kwargs = dict(
 
     # Setup the meta-testing phase and allow it to run.
@@ -85,6 +72,81 @@ meta_test_test_kwargs = dict(
 
     # Run through meta-test testing 5 images at a time. No training occurs here.
     test_test_batch_size=5,
+)
+
+
+# This attempts to replicate the results from the original OML paper.
+# It's missing two additional variations incorporated in the configs below
+#    1) Reserving classes 1 to 480 for the replay set and classes 481 to 962
+#       for the fast/slow sets.
+#    2) Keeping the data unnormalized.
+#
+# Results:
+#
+# |--------------------------------------------------------------|
+# |   Num Classes | Meta-test test   | Meta-test train   |  LR   |
+# |--------------:|:-----------------|:------------------|------:|
+# |            10 | 0.87 ± 0.07      | 0.95 ± 0.03       | 0.001 |
+# |            50 | 0.87 ± 0.02      | 0.96 ± 0.02       | 0.001 |
+# |           100 | 0.81 ± 0.02      | 0.94 ± 0.01       | 0.001 |
+# |           200 | 0.73 ± 0.02      | 0.91 ± 0.02       | 0.001 |
+# |           600 | 0.59 ± 0.01      | 0.85 ± 0.01       | 0.001 |
+# |--------------------------------------------------------------|
+#
+metacl_oml_replicate = deepcopy(metacl_oml)
+metacl_oml_replicate.update(
+
+    # How often to checkpoint (in epochs)
+    checkpoint_freq=1000,
+    keep_checkpoints_num=1,
+    checkpoint_at_end=True,
+    checkpoint_score_attr="training_iteration",
+
+    # Number of tasks (i.e. distinct classes) for the inner loops and outer loop
+    tasks_per_epoch=3,
+
+    # Batch size for each inner and outer step, respectively.
+    batch_size=1,       # use 1 images per step w/in the inner loop
+    slow_batch_size=5,  # use 5 images per class w/in the outer loop.
+
+    # Number of steps per task within the inner loop.
+    num_fast_steps=5,  # => we take 5 * 'tasks_per_epoch' sequential grad steps
+    # num_slow_steps=1,  # not actually defined, but technically this is always one.
+
+    # Number of images sampled for training (should be b/w 1 and 20)
+    train_train_sample_size=20,  # This applies to both the slow and fast iterations.
+
+    # Sampled for the remember set; taken from the whole dataset.
+    # Total images used for one step in the outer loop equals:
+    #      replay_batch_size + (slow_batch_size * tasks_per_epoch) = 30
+    replay_batch_size=15,
+
+    # Since all samples are being used in the slow and fast loops (all 20 of them),
+    # there are none being held out for a separate validation phase.
+    val_batch_size=1,  # for the validation set; not being used.
+
+    # The number of outer (i.e. slow) steps. The OML-repo README recommends 700000,
+    # but we use less here. Note, the OML-repo results above report 20000 steps as well
+    # for a consistent comparison.
+    epochs=20000,
+
+    # Identify the params of the output layer.
+    output_layer_params=["adaptation.0.weight", "adaptation.0.bias"],
+
+    # Reset task params in the output layer prior to meta-train training on that task.
+    reset_task_params=True,
+
+    # Whether to run the meta-testing phase at the end of the experiment.
+    run_meta_test=False,  # we won't run this for now
+
+    # Log results to wandb.
+    wandb_args=dict(
+        name="metacl_oml_replicate",
+        project="metacl",
+    ),
+
+    # Meta-testing specific arguments.
+    **deepcopy(meta_test_test_kwargs),
 )
 
 
@@ -178,6 +240,7 @@ oml_datasplit_without_norm.update(
 # ------------
 
 CONFIGS = dict(
+    metacl_oml_replicate=metacl_oml_replicate,
     oml_fastslow_replay_split=oml_fastslow_replay_split,
     oml_without_normalization=oml_without_normalization,
     oml_datasplit_without_norm=oml_datasplit_without_norm,
