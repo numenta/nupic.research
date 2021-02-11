0c7d6ad05ef6fb16cadf8210d5e52885736252a9
Akash Velu <akashvelu@berkeley.edu>
2021-02-10 16:27:17 -0800

    add temperature update mixin for sampled kwinners

--- nupic/research/frameworks/vernon/mixins/__init__.py
+++ nupic/research/frameworks/vernon/mixins/__init__.py
@@ -50,3 +50,4 @@ from .reduce_lr_after_task import ReduceLRAfterTask
 from .vary_batch_size import VaryBatchSize
 from .ewc import ElasticWeightConsolidation
 from .oml import OnlineMetaLearning
+from .update_kwinner_temperature import UpdateKWinnerTemperature
