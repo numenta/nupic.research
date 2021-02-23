32e62b21d4811bc4b23d8335e2e2d1e66497ed02
Alexander Cuozzo <acuozzo@numenta.com>
2021-02-23 14:31:10 -0500

    cleaned code style

--- nupic/research/frameworks/backprop_structure/networks/vdrop_lenet.py
+++ nupic/research/frameworks/backprop_structure/networks/vdrop_lenet.py
@@ -28,118 +28,143 @@ from functools import partial
 
 from torch import nn
 
+from nupic.research.frameworks.backprop_structure.modules.common_layers import (
+    prunable_vdrop_conv2d,
+    prunable_vdrop_linear,
+)
 from nupic.research.frameworks.backprop_structure.modules.vdrop_layers import (
+    MaskedVDropCentralData,
+    VDropCentralData,
     VDropConv2d,
     VDropLinear,
-    VDropCentralData,
-    MaskedVDropCentralData,
-)
-
-from nupic.research.frameworks.backprop_structure.modules.common_layers import (
-    prunable_vdrop_linear,
-    prunable_vdrop_conv2d,
 )
 
 
 class VDropLeNet(nn.Module):
-
-    def __init__(self, input_shape=(1, 32, 32),
-                 cnn_out_channels=(64, 64),
-                 num_classes=12,
-                 use_batch_norm=True,
-                 z_logvar_init=-10,
-                 vdrop_data_class=VDropCentralData,
-                 kernel_size=5,
-                 linear_units=1000,
-                 maxpool_stride=2,
-                 bn_track_running_stats=True,
-                 conv_target_density = (1.0, 1.0),
-                 linear_target_density  = (1.0, 1.0),
-                 ):
+    def __init__(
+        self,
+        input_shape=(1, 32, 32),
+        cnn_out_channels=(64, 64),
+        num_classes=12,
+        use_batch_norm=True,
+        z_logvar_init=-10,
+        vdrop_data_class=VDropCentralData,
+        kernel_size=5,
+        linear_units=1000,
+        maxpool_stride=2,
+        bn_track_running_stats=True,
+        conv_target_density=(1.0, 1.0),
+        linear_target_density=(1.0, 1.0),
+    ):
         feature_map_sidelength = (
-            (((input_shape[1] - kernel_size + 1) / maxpool_stride)
-             - kernel_size + 1) / maxpool_stride
-        )
+            ((input_shape[1] - kernel_size + 1) / maxpool_stride) - kernel_size + 1
+        ) / maxpool_stride
         vdrop_data = vdrop_data_class(z_logvar_init=z_logvar_init)
         super().__init__()
-        assert(feature_map_sidelength == int(feature_map_sidelength))
+        assert feature_map_sidelength == int(feature_map_sidelength)
         feature_map_sidelength = int(feature_map_sidelength)
 
         modules = [
             # -------------
             # Conv Block
             # -------------
-
-            ("vdrop_cnn1", prunable_vdrop_conv2d(input_shape[0],
-                                 cnn_out_channels[0],
-                                 kernel_size,
-                                 vdrop_data,
-                                 target_density=conv_target_density[0])),
+            (
+                "vdrop_cnn1",
+                prunable_vdrop_conv2d(
+                    input_shape[0],
+                    cnn_out_channels[0],
+                    kernel_size,
+                    vdrop_data,
+                    target_density=conv_target_density[0],
+                ),
+            ),
             ("cnn1_maxpool", nn.MaxPool2d(maxpool_stride)),
         ]
 
         if use_batch_norm:
             modules.append(
-                ("cnn1_bn", nn.BatchNorm2d(
-                    cnn_out_channels[0],
-                    affine=False,
-                    track_running_stats=bn_track_running_stats)))
+                (
+                    "cnn1_bn",
+                    nn.BatchNorm2d(
+                        cnn_out_channels[0],
+                        affine=False,
+                        track_running_stats=bn_track_running_stats,
+                    ),
+                )
+            )
 
         modules += [
             ("cnn1_relu", nn.ReLU(inplace=True)),
-
             # -------------
             # Conv Block
             # -------------
-
-            ("vdrop_cnn2", prunable_vdrop_conv2d(cnn_out_channels[0],
-                                 cnn_out_channels[1],
-                                 kernel_size,
-                                 vdrop_data,
-                                 target_density=conv_target_density[1])),
+            (
+                "vdrop_cnn2",
+                prunable_vdrop_conv2d(
+                    cnn_out_channels[0],
+                    cnn_out_channels[1],
+                    kernel_size,
+                    vdrop_data,
+                    target_density=conv_target_density[1],
+                ),
+            ),
             ("cnn2_maxpool", nn.MaxPool2d(maxpool_stride)),
         ]
 
         if use_batch_norm:
             modules.append(
-                ("cnn2_bn", nn.BatchNorm2d(
-                    cnn_out_channels[1],
-                    affine=False,
-                    track_running_stats=bn_track_running_stats)))
-
+                (
+                    "cnn2_bn",
+                    nn.BatchNorm2d(
+                        cnn_out_channels[1],
+                        affine=False,
+                        track_running_stats=bn_track_running_stats,
+                    ),
+                )
+            )
 
         modules += [
             ("cnn2_relu", nn.ReLU(inplace=True)),
             ("flatten", nn.Flatten()),
-
             # -------------
             # Linear Block
             # -------------
-
-            ("vdrop_fc1", prunable_vdrop_linear(
-                (feature_map_sidelength**2) * cnn_out_channels[1],
-                linear_units,
-                vdrop_data,
-                target_density=linear_target_density[0])),
+            (
+                "vdrop_fc1",
+                prunable_vdrop_linear(
+                    (feature_map_sidelength ** 2) * cnn_out_channels[1],
+                    linear_units,
+                    vdrop_data,
+                    target_density=linear_target_density[0],
+                ),
+            ),
         ]
         if use_batch_norm:
             modules.append(
-                ("fc1_bn", nn.BatchNorm1d(
-                    linear_units,
-                    affine=False,
-                    track_running_stats=bn_track_running_stats)))
+                (
+                    "fc1_bn",
+                    nn.BatchNorm1d(
+                        linear_units,
+                        affine=False,
+                        track_running_stats=bn_track_running_stats,
+                    ),
+                )
+            )
 
         modules += [
             ("fc1_relu", nn.ReLU(inplace=True)),
-
             # -------------
             # Output Layer
             # -------------
-
-            ("vdrop_fc2", prunable_vdrop_linear(linear_units,
-                                                num_classes,
-                                                vdrop_data,
-                                                target_density=linear_target_density[1])),
+            (
+                "vdrop_fc2",
+                prunable_vdrop_linear(
+                    linear_units,
+                    num_classes,
+                    vdrop_data,
+                    target_density=linear_target_density[1],
+                ),
+            ),
         ]
         self.classifier = nn.Sequential(OrderedDict(modules))
         vdrop_data.finalize()
@@ -157,26 +182,22 @@ class VDropLeNet(nn.Module):
         return ret
 
 
-gsc_lenet_vdrop = partial(
-    VDropLeNet,
-    input_shape=(1, 32, 32),
-    num_classes=12
-)
+gsc_lenet_vdrop = partial(VDropLeNet, input_shape=(1, 32, 32), num_classes=12)
 
 gsc_lenet_vdrop_sparse = partial(
     VDropLeNet,
-    vdrop_data_class = MaskedVDropCentralData,
+    vdrop_data_class=MaskedVDropCentralData,
     input_shape=(1, 32, 32),
     num_classes=12,
     linear_target_density=(0.1, 0.1),
-    conv_target_density = (0.1, 0.1)
+    conv_target_density=(0.1, 0.1),
 )
 
 gsc_lenet_vdrop_super_sparse = partial(
     VDropLeNet,
-    vdrop_data_class = MaskedVDropCentralData,
+    vdrop_data_class=MaskedVDropCentralData,
     input_shape=(1, 32, 32),
     num_classes=12,
     linear_target_density=(0.01, 0.01),
-    conv_target_density = (0.01, 0.01)
+    conv_target_density=(0.01, 0.01),
 )
--- projects/gsc/experiments/gsc_vdrop_pruning.py
+++ projects/gsc/experiments/gsc_vdrop_pruning.py
@@ -24,30 +24,36 @@ Run a simple GSC experiment using variational dropout modules.
 
 from copy import deepcopy
 
-import torch
 import numpy as np
+import torch
+
+from nupic.research.frameworks.backprop_structure.networks import (
+    VDropLeNet,
+    gsc_lenet_vdrop_sparse,
+    gsc_lenet_vdrop_super_sparse,
+)
+from nupic.research.frameworks.pytorch.model_utils import count_nonzero_params
+from nupic.research.frameworks.sigopt.sigopt_experiment import SigOptExperiment
+from nupic.research.frameworks.vernon.distributed import experiments, mixins
 
 from .base import DEFAULT_BASE
-from nupic.research.frameworks.backprop_structure.networks import VDropLeNet, \
-    gsc_lenet_vdrop_sparse, gsc_lenet_vdrop_super_sparse
-from nupic.research.frameworks.vernon.distributed import experiments, mixins
-from nupic.research.frameworks.sigopt.sigopt_experiment import SigOptExperiment
-from nupic.research.frameworks.pytorch.model_utils import count_nonzero_params
+import os
 
 
-class GSCVDropExperiment(mixins.RegularizeLoss,
-                        mixins.ConstrainParameters,
-                        mixins.LogBackpropStructure,
-                        mixins.LogEveryLoss,
-                        mixins.LogEveryLearningRate,
-                        experiments.SupervisedExperiment):
-
+class GSCVDropExperiment(
+    mixins.RegularizeLoss,
+    mixins.ConstrainParameters,
+    mixins.LogBackpropStructure,
+    mixins.LogEveryLoss,
+    mixins.LogEveryLearningRate,
+    experiments.SupervisedExperiment,
+):
     def setup_experiment(self, config):
         super().setup_experiment(config)
-        self.warmup_start_iteration = config.get('warmup_start_iteration', 50)
-        self.warmup_end_iteration = config.get('warmup_end_iteration', 80)
-        self.gamma_warmup = config.get('gamma_warmup', 0.5)
-        self.gamma_postwarmup = config.get('gamma_postwarmup', 0.9)
+        self.warmup_start_iteration = config.get("warmup_start_iteration", 50)
+        self.warmup_end_iteration = config.get("warmup_end_iteration", 80)
+        self.gamma_warmup = config.get("gamma_warmup", 0.5)
+        self.gamma_postwarmup = config.get("gamma_postwarmup", 0.9)
 
     def pre_epoch(self):
         super().pre_epoch()
@@ -64,8 +70,6 @@ REG_FACTOR_END_EPOCH = 80
 LR = 0.0007980119944707181
 
 
-
-
 def get_reg_scalar(epoch, batch_idx, total_batches):
     # Set to reg_factor_start before reg_factor_start_epoch, then scale linearly
     # until reaching reg_factor_end at reg_factor_end_epoch, then hold constant
@@ -73,63 +77,56 @@ def get_reg_scalar(epoch, batch_idx, total_batches):
         return REG_FACTOR_START
     elif epoch > REG_FACTOR_END_EPOCH:
         return REG_FACTOR_END
-    return (epoch - REG_FACTOR_START_EPOCH) / (REG_FACTOR_END_EPOCH -
-                                               REG_FACTOR_START_EPOCH)  * (
-        REG_FACTOR_END - REG_FACTOR_START) + REG_FACTOR_START
-
+    return (epoch - REG_FACTOR_START_EPOCH) / (
+        REG_FACTOR_END_EPOCH - REG_FACTOR_START_EPOCH
+    ) * (REG_FACTOR_END - REG_FACTOR_START) + REG_FACTOR_START
 
 
 GSC_VDROP = deepcopy(DEFAULT_BASE)
 GSC_VDROP.update(
-    experiment_class = GSCVDropExperiment,
+    experiment_class=GSCVDropExperiment,
     # Training batch size
     batch_size=32,
     # Validation batch size
     val_batch_size=32,
-    reg_scalar = get_reg_scalar,
-    gamma_warmup = 0.72,
-    gamma_postwarmup = 0.568,
-    warmup_start_iteration = 50,
-    warmup_end_iteration = 60,
-    wandb_args = dict(
+    reg_scalar=get_reg_scalar,
+    gamma_warmup=0.72,
+    gamma_postwarmup=0.568,
+    warmup_start_iteration=50,
+    warmup_end_iteration=60,
+    wandb_args=dict(
         name="gsc_vdrop_2",
         project="gsc_vdrop_experiments",
         notes="This is a cool experiment",
     ),
-
     lr_scheduler_class=torch.optim.lr_scheduler.StepLR,
-    lr_scheduler_args=dict(
-        step_size=1,
-        gamma=1.0,
-    ),
-    optimizer_args=dict(
-        lr=LR,
-    ),
-    num_epochs= 100,
-    epochs_to_validate = range(100),
-    model_class = VDropLeNet,
-    downscale_reg_with_training_set = True,
-    model_args = dict(),
+    lr_scheduler_args=dict(step_size=1, gamma=1.0),
+    optimizer_args=dict(lr=LR),
+    num_epochs=100,
+    epochs_to_validate=range(100),
+    model_class=VDropLeNet,
+    downscale_reg_with_training_set=True,
+    model_args=dict(),
 )
 
 
 class SNRPruningGSCVDrop(
-
-        mixins.LogEveryLoss,
-        mixins.LogBackpropStructure,
-        mixins.LogEveryLearningRate,
-        mixins.ExtraValidationsPerEpoch,
-
-        mixins.RegularizeLoss,
-        mixins.ConstrainParameters,
-        mixins.MultiCycleLR,
-        mixins.PruneLowSNR,
-
-        experiments.SupervisedExperiment,):
+    mixins.LogEveryLoss,
+    mixins.LogBackpropStructure,
+    mixins.LogEveryLearningRate,
+    mixins.ExtraValidationsPerEpoch,
+    mixins.RegularizeLoss,
+    mixins.ConstrainParameters,
+    mixins.MultiCycleLR,
+    mixins.PruneLowSNR,
+    experiments.SupervisedExperiment,
+):
     pass
 
-def make_reg_schedule(epochs, pct_ramp_start, pct_ramp_end, peak_value,
-                      pct_drop, final_value):
+
+def make_reg_schedule(
+    epochs, pct_ramp_start, pct_ramp_end, peak_value, pct_drop, final_value
+):
     def reg_schedule(epoch, batch_idx, steps_per_epoch):
         pct = (epoch + batch_idx / steps_per_epoch) / epochs
 
@@ -145,6 +142,7 @@ def make_reg_schedule(epochs, pct_ramp_start, pct_ramp_end, peak_value,
 
     return reg_schedule
 
+
 SUBSEQUENT_LR_SCHED_ARGS = dict(
     max_lr=6.0,
     pct_start=0.0625,
@@ -152,133 +150,127 @@ SUBSEQUENT_LR_SCHED_ARGS = dict(
     base_momentum=0.6,
     max_momentum=0.75,
     cycle_momentum=True,
-    final_div_factor=1000.0
+    final_div_factor=1000.0,
 )
 
 NUM_EPOCHS = 120
 
 
-#94% accuracy, ~170k params = 10% sparsity
+# 94% accuracy, ~170k params = 10% sparsity
 GSC_VDROP_SNR_PRUNING = deepcopy(GSC_VDROP)
-GSC_VDROP_SNR_PRUNING.update(dict(
-    name="GSC_VDROP_INITIAL_MULTICYCLE",
-    experiment_class = SNRPruningGSCVDrop,
-    epochs_to_validate=range(1, NUM_EPOCHS),
-    epochs=NUM_EPOCHS,
-    wandb_args=dict(
-        project="gsc-snr-pruning",
-        name="3rd-run-log-sparsity",
-        notes="just a demo to see if the multi-cycle LR w/ pruning is working"
-    ),
-    log_timestep_freq=5,
-    validate_on_prune=True,
-
-    model_class = gsc_lenet_vdrop_sparse,
-
-    multi_cycle_lr_args=(
-        (0, dict(max_lr=6.0,
-                 pct_start=0.2,
-                 anneal_strategy="linear",
-                 base_momentum=0.6,
-                 max_momentum=0.75,
-                 cycle_momentum=True,
-                 div_factor=6.0,
-                 final_div_factor=1000.0)),
-        (30, SUBSEQUENT_LR_SCHED_ARGS),
-        (33, SUBSEQUENT_LR_SCHED_ARGS),
-        (36, SUBSEQUENT_LR_SCHED_ARGS),
-        (39, SUBSEQUENT_LR_SCHED_ARGS),
-        (42, SUBSEQUENT_LR_SCHED_ARGS),
-        (45, SUBSEQUENT_LR_SCHED_ARGS),
-        (80, SUBSEQUENT_LR_SCHED_ARGS),
-    ),
-
-    reg_scalar=make_reg_schedule(
+GSC_VDROP_SNR_PRUNING.update(
+    dict(
+        name="GSC_VDROP_INITIAL_MULTICYCLE",
+        experiment_class=SNRPruningGSCVDrop,
+        epochs_to_validate=range(1, NUM_EPOCHS),
         epochs=NUM_EPOCHS,
-        pct_ramp_start=10 / 120,
-        pct_ramp_end=30 / 60,
-        peak_value=0.01,
-        pct_drop=45 / 60,
-        final_value=0.0005,
-    ),
-
-    prune_schedule=[
-        (30, 1 / 6),
-        (33, 2 / 6),
-        (36, 3 / 6),
-        (39, 4 / 6),
-        (42, 5 / 6),
-        (45, 6 / 6),
-    ],
-))
-
+        wandb_args=dict(
+            project="gsc-snr-pruning",
+            name="3rd-run-log-sparsity",
+            notes="just a demo to see if the multi-cycle LR w/ pruning is working",
+        ),
+        log_timestep_freq=5,
+        validate_on_prune=True,
+        model_class=gsc_lenet_vdrop_sparse,
+        multi_cycle_lr_args=(
+            (
+                0,
+                dict(
+                    max_lr=6.0,
+                    pct_start=0.2,
+                    anneal_strategy="linear",
+                    base_momentum=0.6,
+                    max_momentum=0.75,
+                    cycle_momentum=True,
+                    div_factor=6.0,
+                    final_div_factor=1000.0,
+                ),
+            ),
+            (30, SUBSEQUENT_LR_SCHED_ARGS),
+            (33, SUBSEQUENT_LR_SCHED_ARGS),
+            (36, SUBSEQUENT_LR_SCHED_ARGS),
+            (39, SUBSEQUENT_LR_SCHED_ARGS),
+            (42, SUBSEQUENT_LR_SCHED_ARGS),
+            (45, SUBSEQUENT_LR_SCHED_ARGS),
+            (80, SUBSEQUENT_LR_SCHED_ARGS),
+        ),
+        reg_scalar=make_reg_schedule(
+            epochs=NUM_EPOCHS,
+            pct_ramp_start=10 / 120,
+            pct_ramp_end=30 / 60,
+            peak_value=0.01,
+            pct_drop=45 / 60,
+            final_value=0.0005,
+        ),
+        prune_schedule=[
+            (30, 1 / 6),
+            (33, 2 / 6),
+            (36, 3 / 6),
+            (39, 4 / 6),
+            (42, 5 / 6),
+            (45, 6 / 6),
+        ],
+    )
+)
 
 
 GSC_VDROP_SNR_PRUNING_SUPER_SPARSE = deepcopy(GSC_VDROP_SNR_PRUNING)
-GSC_VDROP_SNR_PRUNING_SUPER_SPARSE.update(dict(
-    name="GSC_VDROP_SNR_PRUNING_SUPER_SPARSE",
-    wandb_args=dict(
-        project="gsc-snr-pruning-sparse",
-        name="very_sparse",
-        notes="how sparse can it get?"
-    ),
-    model_class = gsc_lenet_vdrop_super_sparse,
+GSC_VDROP_SNR_PRUNING_SUPER_SPARSE.update(
+    dict(
+        name="GSC_VDROP_SNR_PRUNING_SUPER_SPARSE",
+        wandb_args=dict(
+            project="gsc-snr-pruning-sparse",
+            name="very_sparse",
+            notes="how sparse can it get?",
+        ),
+        model_class=gsc_lenet_vdrop_super_sparse,
+        multi_cycle_lr_args=(
+            (
+                0,
+                dict(
+                    max_lr=6.0,
+                    pct_start=0.2,
+                    anneal_strategy="linear",
+                    base_momentum=0.6,
+                    max_momentum=0.75,
+                    cycle_momentum=True,
+                    div_factor=6.0,
+                    final_div_factor=1000.0,
+                ),
+            ),
+            (30, SUBSEQUENT_LR_SCHED_ARGS),
+            (33, SUBSEQUENT_LR_SCHED_ARGS),
+            (36, SUBSEQUENT_LR_SCHED_ARGS),
+            (39, SUBSEQUENT_LR_SCHED_ARGS),
+            (42, SUBSEQUENT_LR_SCHED_ARGS),
+            (46, SUBSEQUENT_LR_SCHED_ARGS),
+            (50, SUBSEQUENT_LR_SCHED_ARGS),
+            (54, SUBSEQUENT_LR_SCHED_ARGS),
+            (80, SUBSEQUENT_LR_SCHED_ARGS),
+        ),
+        reg_scalar=make_reg_schedule(
+            epochs=NUM_EPOCHS,
+            pct_ramp_start=10 / 120,
+            pct_ramp_end=30 / 60,
+            peak_value=0.01,
+            pct_drop=54 / 120,
+            final_value=0.0005,
+        ),
+        prune_schedule=[
+            (30, 1 / 8),
+            (33, 2 / 8),
+            (36, 3 / 8),
+            (39, 4 / 8),
+            (42, 5 / 8),
+            (46, 6 / 8),
+            (50, 7 / 8),
+            (54, 8 / 8),
+        ],
+    )
+)
 
-    multi_cycle_lr_args=(
-        (0, dict(max_lr=6.0,
-                 pct_start=0.2,
-                 anneal_strategy="linear",
-                 base_momentum=0.6,
-                 max_momentum=0.75,
-                 cycle_momentum=True,
-                 div_factor=6.0,
-                 final_div_factor=1000.0)),
-        (30, SUBSEQUENT_LR_SCHED_ARGS),
-        (33, SUBSEQUENT_LR_SCHED_ARGS),
-        (36, SUBSEQUENT_LR_SCHED_ARGS),
-        (39, SUBSEQUENT_LR_SCHED_ARGS),
-        (42, SUBSEQUENT_LR_SCHED_ARGS),
-        (46, SUBSEQUENT_LR_SCHED_ARGS),
-        (50, SUBSEQUENT_LR_SCHED_ARGS),
-        (54, SUBSEQUENT_LR_SCHED_ARGS),
-        (80, SUBSEQUENT_LR_SCHED_ARGS),
-    ),
-    reg_scalar=make_reg_schedule(
-        epochs=NUM_EPOCHS,
-        pct_ramp_start=10 / 120,
-        pct_ramp_end=30 / 60,
-        peak_value=0.01,
-        pct_drop=54/120,
-        final_value=0.0005,
-    ),
-    prune_schedule=[
-        (30, 1 / 8),
-        (33, 2 / 8),
-        (36, 3 / 8),
-        (39, 4 / 8),
-        (42, 5 / 8),
-        (46, 6 / 8),
-        (50, 7 / 8),
-        (54, 8 / 8),
-    ],
-))
 
-
-
-
-
-class SNRPruningGSCVDropSIGOPT(
-        mixins.LogEveryLoss,
-        mixins.LogBackpropStructure,
-        mixins.LogEveryLearningRate,
-        mixins.ExtraValidationsPerEpoch,
-
-        mixins.RegularizeLoss,
-        mixins.ConstrainParameters,
-        mixins.MultiCycleLR,
-        mixins.PruneLowSNR,
-        SigOptExperiment):
-
+class SNRPruningGSCVDropSIGOPT(SigOptExperiment):
     def update_config_with_suggestion(self, config, suggestion):
         """
         Given a SigOpt suggestion, update the optimizer_args with SGD optimizer params.
@@ -324,35 +316,53 @@ class SNRPruningGSCVDropSIGOPT(
             base_momentum=0.6,
             max_momentum=momentum_max_value,
             cycle_momentum=True,
-            final_div_factor=1000.0
+            final_div_factor=1000.0,
         )
-        multi_cycle_lr_args = ((0, dict(max_lr=6.0,
-                                        pct_start=0.2,
-                                        anneal_strategy="linear",
-                                        base_momentum=0.6,
-                                        max_momentum=0.75,
-                                        cycle_momentum=True,
-                                        div_factor=6.0,
-                                        final_div_factor=1000.0)),
-                               ) + tuple([(30 + prune_iteration *
-                                           epochs_per_pruning_cycle, CYCLE_LR_ARGS)
-                                          for prune_iteration in range(
-                num_pruning_iterations)]) + tuple((30 + num_pruning_iterations *
-                                                   epochs_per_pruning_cycle,
-                                                   CYCLE_LR_ARGS))
+        multi_cycle_lr_args = (
+            (
+                (
+                    0,
+                    dict(
+                        max_lr=6.0,
+                        pct_start=0.2,
+                        anneal_strategy="linear",
+                        base_momentum=0.6,
+                        max_momentum=0.75,
+                        cycle_momentum=True,
+                        div_factor=6.0,
+                        final_div_factor=1000.0,
+                    ),
+                ),
+            )
+            + tuple(
+                [
+                    (30 + prune_iteration * epochs_per_pruning_cycle, CYCLE_LR_ARGS)
+                    for prune_iteration in range(num_pruning_iterations)
+                ]
+            )
+            + tuple(
+                (30 + num_pruning_iterations * epochs_per_pruning_cycle, CYCLE_LR_ARGS)
+            )
+        )
         reg_scalar = make_reg_schedule(
             epochs=NUM_EPOCHS,
             pct_ramp_start=10 / num_epochs,
             pct_ramp_end=30 / num_epochs,
             peak_value=reg_scalar_max_value,
-            pct_drop=(
-                                 30 + num_pruning_iterations * epochs_per_pruning_cycle) / num_epochs,
+            pct_drop=(30 + num_pruning_iterations * epochs_per_pruning_cycle)
+            / num_epochs,
             final_value=reg_scalar_final_value,
         )
 
-        prune_schedule = tuple([(30 + prune_iteration * epochs_per_pruning_cycle,
-                                 prune_iteration / num_pruning_iterations) for
-                                prune_iteration in range(num_pruning_iterations)])
+        prune_schedule = tuple(
+            [
+                (
+                    30 + prune_iteration * epochs_per_pruning_cycle,
+                    prune_iteration / num_pruning_iterations,
+                )
+                for prune_iteration in range(num_pruning_iterations)
+            ]
+        )
 
     @classmethod
     def get_execution_order(cls):
@@ -365,7 +375,7 @@ class SNRPruningGSCVDropSIGOPT(
 
 # Initial ~30 epochs one large cycle, followed by pruning iterations
 # how many pruning iterations, and how long is each iteration (num epochs/pruning cycle)
-    # how should the learning rate and momentum change? (how high LR should go)
+# how should the learning rate and momentum change? (how high LR should go)
 # how high should the regularization warmup go?
 
 SIGOPT_GSC_VDROP_SNR_PRUNING = deepcopy(GSC_VDROP_SNR_PRUNING)
@@ -374,43 +384,44 @@ SIGOPT_GSC_VDROP_SNR_PRUNING.update(
     sigopt_config=dict(
         name="sigopt_vdrop_gsc_snr_pruning",
         parameters=[
-            dict(name="num_pruning_iterations", type="int", bounds=dict(min=3,
-                                                                     max=10)),
-            dict(name="epochs_per_pruning_cycle", type="int", bounds=dict(min=2,
-                                                                        max=8)),
-            dict(name="max_lr", type="double", bounds=dict(min=0.001, max=1.5),
-                    transformation="log"),
-            dict(name="reg_scalar_max_value", type="double", bounds=dict(min=0.01,
-                                                                         max=0.3),
-                 transformation="log"),
-            dict(name="reg_scalar_final_value", type="double", dounds=dict(min=0.0001,
-                                                                         max=0.005)),
-            dict(name="momentum_max_value", type="double", dounds=dict(min=0.6,
-                                                                         max=0.9))
+            dict(name="num_pruning_iterations", type="int", bounds=dict(min=3, max=10)),
+            dict(
+                name="epochs_per_pruning_cycle", type="int", bounds=dict(min=2, max=8)
+            ),
+            dict(
+                name="max_lr",
+                type="double",
+                bounds=dict(min=0.001, max=1.5),
+                transformation="log",
+            ),
+            dict(
+                name="reg_scalar_max_value",
+                type="double",
+                bounds=dict(min=0.01, max=0.3),
+                transformation="log",
+            ),
+            dict(
+                name="reg_scalar_final_value",
+                type="double",
+                bounds=dict(min=0.0001, max=0.005),
+            ),
+            dict(
+                name="momentum_max_value", type="double", bounds=dict(min=0.6, max=0.9)
+            ),
         ],
         metrics=[dict(name="mean_accuracy", objective="maximize")],
         parallel_bandwidth=1,
-        observation_budget=10,
+        observation_budget=20,
         project="gsc_snr_pruning",
     ),
-
-    sigopt_experiment_id=None,
-
+    sigopt_experiment_id=374528,
+    api_key = os.environ("SIGOPT_KEY")
 )
-
-
-
-
-
-
-
-
 
 
 CONFIGS = dict(
     gsc_vdrop=GSC_VDROP,
-    gsc_vdrop_snr_pruning = GSC_VDROP_SNR_PRUNING,
-    gsc_vdrop_snr_pruning_super_sparse = GSC_VDROP_SNR_PRUNING_SUPER_SPARSE,
-    sigopt_gsc_vdrop_snr_pruning = SIGOPT_GSC_VDROP_SNR_PRUNING
+    gsc_vdrop_snr_pruning=GSC_VDROP_SNR_PRUNING,
+    gsc_vdrop_snr_pruning_super_sparse=GSC_VDROP_SNR_PRUNING_SUPER_SPARSE,
+    sigopt_gsc_vdrop_snr_pruning=SIGOPT_GSC_VDROP_SNR_PRUNING,
 )
-
