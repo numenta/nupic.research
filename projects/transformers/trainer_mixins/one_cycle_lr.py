c30e8d6c788e8b0f65c5450f39d4e0d037654858
Michaelangelo <mvacaporale@gmail.com>
2021-04-08 18:26:24 -0700

    Move retrieval of args to `create_optimizer_and_scheduler` to enable hp search.

--- projects/transformers/trainer_mixins/one_cycle_lr.py
+++ projects/transformers/trainer_mixins/one_cycle_lr.py
@@ -32,28 +32,25 @@ class OneCycleLRMixin:
         https://pytorch.org/docs/stable/optim.html#torch.optim.lr_scheduler.OneCycleLR
     """
 
-    def __init__(self, *args, **kwargs):
-
-        super().__init__(*args, **kwargs)
-
-        mixin_args = self.args.trainer_mixin_args
-
-        self.max_lr = mixin_args.get("max_lr", 1e-2)
-        self.pct_start = mixin_args.get("pct_start", 0.3)
-        self.anneal_strategy = mixin_args.get("anneal_strategy", "linear")
-        self.cycle_momentum = mixin_args.get("cycle_momentum", True)
-        self.base_momentum = mixin_args.get("base_momentum", 0.85)
-        self.max_momentum = mixin_args.get("max_momentum", 0.95)
-        self.div_factor = mixin_args.get("div_factor", 25)
-        self.final_div_factor = mixin_args.get("final_div_factor", 1e4)
-        self.last_epoch = mixin_args.get("last_epoch", -1)
-
     def create_optimizer_and_scheduler(self, num_training_steps: int):
         """
         Setup the optimizer and the learning rate scheduler. This overrides super
         in a way that just customizes the lr scheduler while the optimizer remains the
         default.
         """
+
+        # Unpack arguments from trainer_mixin_args
+        mixin_args = self.args.trainer_mixin_args
+
+        max_lr = mixin_args.get("max_lr", 1e-2)
+        pct_start = mixin_args.get("pct_start", 0.3)
+        anneal_strategy = mixin_args.get("anneal_strategy", "linear")
+        cycle_momentum = mixin_args.get("cycle_momentum", True)
+        base_momentum = mixin_args.get("base_momentum", 0.85)
+        max_momentum = mixin_args.get("max_momentum", 0.95)
+        div_factor = mixin_args.get("div_factor", 25)
+        final_div_factor = mixin_args.get("final_div_factor", 1e4)
+        last_epoch = mixin_args.get("last_epoch", -1)
 
         # Set lr scheduler to dummy variable so it's not created in the call to super.
         self.lr_scheduler = ...
@@ -65,13 +62,13 @@ class OneCycleLRMixin:
         self.lr_scheduler = OneCycleLR(
             self.optimizer,
             total_steps=num_training_steps,
-            max_lr=self.max_lr,
-            pct_start=self.pct_start,
-            anneal_strategy=self.anneal_strategy,
-            cycle_momentum=self.cycle_momentum,
-            base_momentum=self.base_momentum,
-            max_momentum=self.max_momentum,
-            div_factor=self.div_factor,
-            final_div_factor=self.final_div_factor,
-            last_epoch=self.last_epoch,
+            max_lr=max_lr,
+            pct_start=pct_start,
+            anneal_strategy=anneal_strategy,
+            cycle_momentum=cycle_momentum,
+            base_momentum=base_momentum,
+            max_momentum=max_momentum,
+            div_factor=div_factor,
+            final_div_factor=final_div_factor,
+            last_epoch=last_epoch,
         )
