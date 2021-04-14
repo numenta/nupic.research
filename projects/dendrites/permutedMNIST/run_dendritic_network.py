77effc3c3b163de4581bbec399c4ccd0c46c52ba
karangrewal <kgrewal@numenta.com>
2021-04-13 20:39:59 -0400

    XdG-like experiment on permutedMNIST

---
+++ projects/dendrites/permutedMNIST/run_dendritic_network.py
@@ -0,0 +1,231 @@
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2021, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
+"""
+This module trains & evaluates a dendritic network in a continual learning setting on
+permutedMNIST for a specified number of tasks/permutations. A context vector is
+provided to the dendritic network, so task information need not be inferred.
+
+This setup is very similar to that of context-dependent gating model from the paper
+'Alleviating catastrophic forgetting using contextdependent gating and synaptic
+stabilization' (Masse et al., 2018).
+"""
+
+import math
+from collections import defaultdict
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+from nupic.research.frameworks.dendrites import DendriticMLP
+from nupic.research.frameworks.pytorch.datasets import ContextDependentPermutedMNIST
+from nupic.research.frameworks.vernon import ContinualLearningExperiment, mixins
+
+
+# ------ Experiment class
+class PermutedMNISTExperiment(mixins.RezeroWeights,
+                              ContinualLearningExperiment):
+
+    def setup_experiment(self, config):
+        # Store optimizer args, since optimizer is reset at the beginning of each task
+        self.optimizer_class = config.get("optimizer_class", torch.optim.SGD)
+        self.optimizer_args = config.get("optimizer_args", {})
+
+        super().setup_experiment(config)
+
+    @classmethod
+    def compute_task_indices(cls, config, dataset):
+        # Assume dataloaders are already created
+        class_indices = defaultdict(list)
+        for idx, (_, _, target) in enumerate(dataset):
+            class_indices[target].append(idx)
+
+        # Defines how many classes should exist per task
+        num_tasks = config.get("num_tasks", 1)
+        num_classes = config.get("num_classes", None)
+        assert num_classes is not None, "num_classes should be defined"
+        num_classes_per_task = math.floor(num_classes / num_tasks)
+
+        task_indices = defaultdict(list)
+        for i in range(num_tasks):
+            for j in range(num_classes_per_task):
+                task_indices[i].extend(class_indices[j + (i * num_classes_per_task)])
+        return task_indices
+
+    def run_iteration(self):
+        # Reset optimizer before training on a new task, just as context-dependent
+        # gating does
+        del self.optimizer
+
+        self.optimizer = self.optimizer_class(self.model.parameters(),
+                                              **self.optimizer_args)
+        return super().run_iteration()
+
+
+# ------ Training & evaluation functions
+def train_model(exp):
+    # Assume `loader` yields 3-item tuples of the form (data, context, target)
+    exp.model.train()
+    for data, context, target in exp.train_loader:
+
+        # Since there's only one output head, target values should be modified to be in
+        # the range [0, 1, ..., 9]
+        target = target % exp.num_classes_per_task
+
+        data = data.to(exp.device)
+        context = context.to(exp.device)
+        target = target.to(exp.device)
+
+        exp.optimizer.zero_grad()
+        output = exp.model(data, context)
+
+        output = F.log_softmax(output)
+        error_loss = exp.error_loss(output, target)
+        error_loss.backward()
+        exp.optimizer.step()
+
+        # Rezero weights if necessary
+        exp.post_optimizer_step(exp.model)
+
+
+def evaluate_model(exp):
+    # Assume `loader` yields 3-item tuples of the form (data, context, target)
+    exp.model.eval()
+    total = 0
+
+    loss = torch.tensor(0., device=exp.device)
+    correct = torch.tensor(0, device=exp.device)
+
+    with torch.no_grad():
+
+        for data, context, target in exp.val_loader:
+
+            # Since there's only one output head, target values should be modified to
+            # be in the range [0, 1, ..., 9]
+            target = target % exp.num_classes_per_task
+
+            data = data.to(exp.device)
+            context = context.to(exp.device)
+            target = target.to(exp.device)
+
+            output = exp.model(data, context)
+
+            # All output units are used to compute loss / accuracy
+            loss += exp.error_loss(output, target, reduction="sum")
+            pred = output.max(1, keepdim=True)[1]
+            correct += pred.eq(target.view_as(pred)).sum()
+            total += len(data)
+
+    mean_acc = torch.true_divide(correct, total).item() if total > 0 else 0,
+    return mean_acc
+
+
+def run_experiment(config):
+    exp_class = config["experiment_class"]
+    exp = exp_class()
+    exp.setup_experiment(config)
+
+    exp.model = exp.model.to(exp.device)
+
+    # --------------------------- CONTINUAL LEARNING PHASE -------------------------- #
+
+    for task_id in range(num_tasks):
+
+        # Train model on current task
+        exp.train_loader.sampler.set_active_tasks(task_id)
+        for _epoch_id in range(exp.epochs):
+            train_model(exp)
+
+        if task_id in config["epochs_to_validate"]:
+
+            print("")
+            print(f"=== AFTER TASK {task_id} ===")
+            print("")
+
+            # Evaluate model accuracy on each task separately
+            for eval_task_id in range(task_id + 1):
+
+                exp.val_loader.sampler.set_active_tasks(eval_task_id)
+                acc_task = evaluate_model(exp)
+                if isinstance(acc_task, tuple):
+                    acc_task = acc_task[0]
+
+                print(f"task {eval_task_id} accuracy: {acc_task}")
+            print("")
+
+        else:
+            print(f"--Completed task {task_id}--")
+
+    # ------------------------------------------------------------------------------- #
+
+    # Report final aggregate accuracy
+    exp.val_loader.sampler.set_active_tasks(range(num_tasks))
+    acc_task = evaluate_model(exp)
+    if isinstance(acc_task, tuple):
+        acc_task = acc_task[0]
+
+    print(f"Final test accuracy: {acc_task}")
+    print("")
+
+
+if __name__ == "__main__":
+
+    num_tasks = 50
+
+    config = dict(
+        experiment_class=PermutedMNISTExperiment,
+
+        dataset_class=ContextDependentPermutedMNIST,
+        dataset_args=dict(
+            num_tasks=num_tasks,
+            dim_context=1024,
+        ),
+
+        model_class=DendriticMLP,
+        model_args=dict(
+            input_size=784,
+            output_size=10,
+            hidden_size=2048,
+            num_segments=num_tasks,
+            dim_context=1024,  # Note: with the Gaussian dataset, `dim_context` was
+                               # 2048, but this shouldn't effect results
+            kw=True,
+            dendrite_sparsity=0.0,
+        ),
+
+        batch_size=256,
+        val_batch_size=512,
+        epochs=1,
+        epochs_to_validate=(4, 9, 24, 49),  # Note: `epochs_to_validate` is treated as
+                                            # the set of task ids after which to
+                                            # evaluate the model on all seen tasks
+        num_tasks=num_tasks,
+        num_classes=10 * num_tasks,
+        distributed=False,
+        seed=np.random.randint(0, 10000),
+
+        loss_function=F.nll_loss,
+        optimizer_class=torch.optim.Adam,
+        optimizer_args=dict(lr=0.001),
+    )
+
+    run_experiment(config)
