db1cd03927922d0017c0161f11f2d08cd806bf78
Alexander Cuozzo <acuozzo@numenta.com>
2021-04-19 12:42:12 -0400

    changes to experiment config

--- projects/greedy_infomax/experiments/default_base.py
+++ projects/greedy_infomax/experiments/default_base.py
@@ -19,37 +19,40 @@
 #
 
 
-import os
+import sys
 from copy import deepcopy
 
 import numpy as np
 import torch
-
-from nupic.research.frameworks.vernon.distributed import experiments, mixins
-from torchvision.datasets import STL10, FakeData
-from torchvision import transforms
 from ray import tune
-import sys
-from nupic.research.frameworks.greedy_infomax.models import FullVisionModel
-from nupic.research.frameworks.greedy_infomax.utils.loss_utils import \
-    multiple_cross_entropy
+from torchvision import transforms
+from torchvision.datasets import STL10
 
-class GreedyInfoMaxExperiment(mixins.LogEveryLoss,
-                              experiments.SelfSupervisedExperiment,):
+from nupic.research.frameworks.greedy_infomax.models import FullVisionModel
+from nupic.research.frameworks.greedy_infomax.utils.loss_utils import (
+    multiple_cross_entropy,
+)
+from nupic.research.frameworks.vernon import experiments, mixins
+
+
+class GreedyInfoMaxExperiment(
+    mixins.LogEveryLoss,
+    experiments.SelfSupervisedExperiment,
+):
     pass
 
 
 # get transforms for the dataset
-def get_transforms(eval=False, aug=None):
+def get_transforms(val=False, aug=None):
     trans = []
 
     if aug["randcrop"]:
-        if eval:
+        if val:
             trans.append(transforms.RandomCrop(aug["randcrop"]))
         else:
             trans.append(transforms.CenterCrop(aug["randcrop"]))
 
-    if aug["flip"] and not eval:
+    if aug["flip"] and not val:
         trans.append(transforms.RandomHorizontalFlip())
 
     if aug["grayscale"]:
@@ -76,125 +79,103 @@ aug = {
     "bw_mean": [0.4120],
     "bw_std": [0.2570],
 }
-transform_unsupervised = transform_supervised = get_transforms(eval=False, aug=aug)
-transform_valid = trans = get_transforms(eval=True, aug=aug)
+transform_unsupervised = transform_supervised = get_transforms(val=False, aug=aug)
+transform_validation = trans = get_transforms(val=True, aug=aug)
 
 
-base_dataset_args=dict(
-        root="~/nta/data/STL10",
-        download=False,
-)
+base_dataset_args = dict(root="~/nta/data/STL10", download=False)
 
 # #fake data class for debugging purposes
 # def fake_data(size=256, image_size=(3, 96, 96), num_classes = 10, train=True,
-#               transform=transform_valid):
+#               transform=transform_validation):
 #     return FakeData(size=size, image_size=image_size, num_classes=num_classes,
 #     transform=transform)
 
 unsupervised_dataset_args = deepcopy(base_dataset_args)
-unsupervised_dataset_args.update(dict(transform=transform_unsupervised,
-                                      split="unlabeled"))
+unsupervised_dataset_args.update(
+    dict(transform=transform_unsupervised, split="unlabeled")
+)
 supervised_dataset_args = deepcopy(base_dataset_args)
-supervised_dataset_args.update(dict(transform=transform_supervised,
-                                      split="train"))
+supervised_dataset_args.update(dict(transform=transform_supervised, split="train"))
 validation_dataset_args = deepcopy(base_dataset_args)
-validation_dataset_args.update(dict(transform=transform_valid,
-                                      split="test"))
+validation_dataset_args.update(dict(transform=transform_validation, split="test"))
 
 
 BATCH_SIZE = 32
 NUM_CLASSES = 10
 DEFAULT_BASE = dict(
     experiment_class=GreedyInfoMaxExperiment,
-
     # Dataset
     dataset_class=STL10,
-    dataset_args=dict(unsupervised=unsupervised_dataset_args,
-                      supervised=supervised_dataset_args,
-                      validation=validation_dataset_args),
-
-    num_unsupervised_samples = 1000,
+    dataset_args=dict(
+        unsupervised=unsupervised_dataset_args,
+        supervised=supervised_dataset_args,
+        validation=validation_dataset_args,
+    ),
+    num_unsupervised_samples=32,
+    num_supervised_samples=32,
+    num_validation_samples=32,
     workers=1,
-    reuse_actors = True,
+    reuse_actors=True,
     # Seed
     seed=tune.sample_from(lambda spec: np.random.randint(1, 10000)),
-
     # Number of times to sample from the hyperparameter space. If `grid_search` is
     # provided the grid will be repeated `num_samples` of times.
-
     # Training batch size
     batch_size=BATCH_SIZE,
     # Validation batch size
     val_batch_size=10000,
     # Number of batches per epoch. Useful for debugging
     batches_in_epoch=sys.maxsize,
-
     # Update this to stop training when accuracy reaches the metric value
     # For example, stop=dict(mean_accuracy=0.75),
     stop=dict(),
-
     # Number of epochs
     epochs=10,
-    epochs_to_validate=[3,],
-
+    epochs_to_validate=[3],
     # Which epochs to run and report inference over the validation dataset.
     # epochs_to_validate=range(-1, 30),  # defaults to the last 3 epochs
-
     # Model class. Must inherit from "torch.nn.Module"
     model_class=FullVisionModel,
-    #default model arguments
-    model_args = dict(negative_samples=10,
-                      k_predictions=5,
-                      resnet_50=False,
-                      grayscale=True,
-                      patch_size=16,
-                      overlap=2),
-
-    classifier_model_class = torch.nn.Linear,
-    classifier_model_args = dict(in_features=100,
-                                 out_features=NUM_CLASSES),
-
-    loss_function = multiple_cross_entropy, #each GIM layer has a cross-entropy
-    loss_function_supervised = torch.nn.functional.cross_entropy,
-
+    # default model arguments
+    model_args=dict(
+        negative_samples=10,
+        k_predictions=5,
+        resnet_50=False,
+        grayscale=True,
+        patch_size=16,
+        overlap=2,
+    ),
+    classifier_config=dict(
+        model_class=torch.nn.Linear,
+        model_args=dict(in_features=100, out_features=NUM_CLASSES),
+        loss_function=torch.nn.functional.cross_entropy,
+        # Classifier Optimizer class. Must inherit from "torch.optim.Optimizer"
+        optimizer_class=torch.optim.Adam,
+        # Optimizer class class arguments passed to the constructor
+        optimizer_args=dict(lr=1.5e-4),
+    ),
+    loss_function=multiple_cross_entropy,  # each GIM layer has a cross-entropy
     # Optimizer class. Must inherit from "torch.optim.Optimizer"
     optimizer_class=torch.optim.Adam,
-
     # Optimizer class class arguments passed to the constructor
-    optimizer_args=dict(
-        lr=1.5e-4,
-    ),
-
-    # Classifier Optimizer class. Must inherit from "torch.optim.Optimizer"
-    classifier_optimizer_class=torch.optim.Adam,
-    # Optimizer class class arguments passed to the constructor
-    classifier_optimizer_args=dict(
-        lr=1.5e-4,
-    ),
-
-
+    optimizer_args=dict(lr=1.5e-4),
     # # Learning rate scheduler class. Must inherit from "_LRScheduler"
     # lr_scheduler_class=torch.optim.lr_scheduler.StepLR,
-
     # How often to checkpoint (epochs)
     checkpoint_freq=0,
     keep_checkpoints_num=1,
     checkpoint_at_end=True,
     checkpoint_score_attr="training_iteration",
-
     # How many times to try to recover before stopping the trial
     max_failures=3,
-
     # How many times to retry the epoch before stopping. This is useful when
     # using distributed training with spot instances.
     max_retries=3,
-
     # Python Logging level : "critical", "error", "warning", "info", "debug"
     log_level="debug",
-
     # Python Logging Format
     log_format="%(asctime)s:%(levelname)s:%(name)s:%(message)s",
-
     # Ray tune verbosity. When set to the default value of 2 it will log
     # iteration result dicts. This dict can flood the console if it contains
     # large data structures, so default to verbose=1. The SupervisedTrainable logs
@@ -203,6 +184,4 @@ DEFAULT_BASE = dict(
 )
 
 
-CONFIGS = dict(
-    default_base = DEFAULT_BASE
-)
+CONFIGS = dict(default_base=DEFAULT_BASE)
