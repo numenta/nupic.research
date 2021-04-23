3b7b91e993272659a8adc1a5366543feec7a2c97
Michaelangelo <mvacaporale@gmail.com>
2021-04-23 12:22:31 -0700

    Update configs.

--- projects/transformers/experiments/trifecta.py
+++ projects/transformers/experiments/trifecta.py
@@ -30,8 +30,9 @@ from trainer_mixins import (
     RigLMixin,
 )
 
+from .finetuning import finetuning_bert700k_glue
 from .sparse_bert import fully_static_sparse_bert_100k_fp16
-from .sparse_bertitos import tiny_bert_sparse_100k
+from .sparse_bertitos import small_bert_sparse_100k, tiny_bert_sparse_100k
 
 
 """
@@ -66,7 +67,7 @@ tiny_bert_trifecta_300k.update(
     model_type="fully_static_sparse_bert",
     overwrite_output_dir=True,
 
-    # RigL
+    # Sparsity callback
     trainer_callbacks=[
         RezeroWeightsCallback(),
         PlotDensitiesCallback(plot_freq=1000),
@@ -96,10 +97,6 @@ tiny_bert_trifecta_300k.update(
         prune_fraction=0.3,
         prune_freq=100,
     ),
-
-    teacher_models_name_or_path=[
-        "bert-base-cased"
-    ],
 )
 
 
@@ -125,6 +122,76 @@ tiny_bert_trifecta_lr_range_test.update(
     do_eval=True,
 )
 
+# ---------
+# Small BERT
+# ---------
+
+# This combines KD + RigL + OneCycle LR on Small BERT.
+# This gets a NaN eval-loss for max_lr=0.006
+small_bert_trifecta_300k = deepcopy(small_bert_sparse_100k)
+small_bert_trifecta_300k.update(
+    max_steps=300000,
+    model_type="fully_static_sparse_bert",
+    overwrite_output_dir=True,
+
+    # Using batch_size of 16 instead of 128 since we're training on 8 GPUs.
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+
+    # Sparsity callback
+    trainer_callbacks=[
+        RezeroWeightsCallback(),
+    ],
+    fp16=True,
+
+    trainer_class=KDRigLOneCycleLRTrainer,
+    trainer_mixin_args=dict(
+
+        # One cycle lr
+        max_lr=0.0025,
+        pct_start=0.10,
+        anneal_strategy="linear",
+        cycle_momentum=True,
+        base_momentum=0.85,
+        max_momentum=0.95,
+        div_factor=25,
+        final_div_factor=1e4,
+        last_epoch=-1,
+
+        # KD
+        teacher_model_names_or_paths=[
+            "/mnt/efs/results/pretrained-models/transformers-local/bert_1mi",
+        ],
+
+        # RigL
+        prune_fraction=0.3,
+        prune_freq=100,
+    ),
+)
+
+
+# LR Range Test for training with KD and OneCycle LR. It's assumed the observed max_lr
+# will carry over to training with RigL.
+small_bert_trifecta_lr_range_test = deepcopy(small_bert_trifecta_300k)
+small_bert_trifecta_lr_range_test.update(
+    max_steps=100,
+    trainer_class=KDLRRangeTestTrainer,
+
+    trainer_mixin_args=dict(
+        # LR Range Test
+        min_lr=0.0001,
+        max_lr=0.05,
+        test_mode="linear",
+
+        # KD
+        teacher_model_names_or_paths=[
+            "/mnt/efs/results/pretrained-models/transformers-local/bert_1mi",
+        ],
+    ),
+    overwrite_output_dir=True,
+    do_eval=True,
+)
+
 
 # ---------
 # BERT Base
@@ -132,6 +199,9 @@ tiny_bert_trifecta_lr_range_test.update(
 
 
 # BERT Base with KD + RigL + OneCycle LR
+# This achieves and eval-loss of 2.138, just slightly under 2.154 from its dense
+# counterpart. See `sparse_v5_trifecta_100k` in the README for more details.
+# This took 18h 21m on four p3dn.24xlarges.
 bert_sparse_trifecta_100k = deepcopy(fully_static_sparse_bert_100k_fp16)
 bert_sparse_trifecta_100k.update(
     trainer_class=KDRigLOneCycleLRTrainer,
@@ -161,11 +231,29 @@ bert_sparse_trifecta_100k.update(
 )
 
 
+# This fine-tunes a pretrained model from `bert_sparse_100k_kd_oncycle_lr` above.
+finetuning_bert_sparse_trifecta_100k_glue = deepcopy(finetuning_bert700k_glue)
+finetuning_bert_sparse_trifecta_100k_glue.update(
+    # Model arguments
+    model_type="fully_static_sparse_bert",
+    model_name_or_path="/mnt/efs/results/pretrained-models/transformers-local/bert_sparse_trifecta_100k",  # noqa: E501
+    task_name=None,
+    # task_names=["sst2", "qnli", "qqp", "mnli"],
+    task_names=["rte", "stsb", "mrpc", "cola"],
+    override_finetuning_results=False,
+)
+
+
 CONFIGS = dict(
     # Tiny BERT
     tiny_bert_trifecta_300k=tiny_bert_trifecta_300k,
     tiny_bert_trifecta_lr_range_test=tiny_bert_trifecta_lr_range_test,
 
+    # Small BERT
+    small_bert_trifecta_300k=small_bert_trifecta_300k,
+    small_bert_trifecta_lr_range_test=small_bert_trifecta_lr_range_test,
+
     # BERT Base
     bert_sparse_trifecta_100k=bert_sparse_trifecta_100k,
+    finetuning_bert_sparse_trifecta_100k_glue=finetuning_bert_sparse_trifecta_100k_glue,
 )
