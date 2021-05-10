58db8ddbfa14dde716c47ccd625196f57e6d0168
Subutai Ahmad <sahmad@numenta.com>
2021-05-10 07:54:22 -0700

    Base parameter searches

--- projects/dendrites/permutedMNIST/experiments/base.py
+++ projects/dendrites/permutedMNIST/experiments/base.py
@@ -25,6 +25,7 @@ Base Experiment configuration.
 import os
 from copy import deepcopy
 
+import numpy as np
 import ray.tune as tune
 import torch
 import torch.nn.functional as F
@@ -44,6 +45,7 @@ class PermutedMNISTExperiment(mixins.RezeroWeights,
 
 NUM_TASKS = 2
 
+# A relatively quick running experiment for debugging
 DEFAULT_BASE = dict(
     experiment_class=PermutedMNISTExperiment,
 
@@ -93,8 +95,112 @@ BASE2.update(
     epochs=tune.grid_search([1, 2, 3]),
 )
 
+# 10 tasks. Currently several variables depend on tasks and are hardcoded in config
+BASE_10_TASKS = deepcopy(DEFAULT_BASE)
+BASE_10_TASKS.update(
+    dataset_args=dict(
+        num_tasks=10,        # NUM_TASKS
+        root=os.path.expanduser("~/nta/results/data/"),
+        dim_context=1024,
+        seed=42,
+        download=False,  # Change to True if running for the first time
+    ),
+
+    epochs=tune.sample_from(lambda spec: np.random.randint(1, 4)),
+    tasks_to_validate=range(100),  # Tasks on which to run validate
+
+    model_args=dict(
+        input_size=784,
+        output_size=10,
+        hidden_sizes=[2048, 2048],
+        num_segments=10,    # NUM_TASKS
+        dim_context=1024,
+        kw=True,
+        dendrite_weight_sparsity=0.0,
+        weight_sparsity=tune.sample_from(
+            lambda spec: np.random.choice([0.95, 0.90, 0.8])),
+    ),
+
+    num_tasks=10,            # NUM_TASKS
+    num_classes=100,         # 10 * NUM_TASKS
+    seed=tune.sample_from(lambda spec: np.random.randint(2, 10000)),
+    num_samples=20,
+
+    optimizer_class=tune.grid_search([torch.optim.Adam, torch.optim.SGD]),
+    optimizer_args=dict(
+        lr=tune.sample_from(
+            lambda spec: np.random.choice([0.01, 0.005, 0.001, 0.0005])),
+    ),
+)
+
+# 10 tasks. Currently several variables depend on tasks and are hardcoded in config
+BASE_10_SPARSITY_SEARCH = deepcopy(BASE_10_TASKS)
+BASE_10_SPARSITY_SEARCH.update(
+    model_args=dict(
+        input_size=784,
+        output_size=10,
+        hidden_sizes=[2048, 2048],
+        num_segments=10,    # NUM_TASKS
+        dim_context=1024,
+        kw=True,
+        kw_percent_on=tune.sample_from(
+            lambda spec: np.random.choice([0.05, 0.1, 0.2, 0.0])),
+        dendrite_weight_sparsity=0.0,
+        weight_sparsity=tune.sample_from(
+            lambda spec: np.random.choice([0.90, 0.8, 0.7, 0.5, 0.25, 0.0])),
+    ),
+
+    epochs=tune.sample_from(lambda spec: np.random.randint(1, 3)),
+    num_samples=5,
+
+    optimizer_class=torch.optim.Adam,
+    optimizer_args=dict(
+        lr=tune.sample_from(lambda spec: np.random.choice([0.001, 0.0005])),
+    ),
+)
+
+# 50 tasks. Currently several variables depend on tasks and are hardcoded in config
+BASE_50_SPARSITY_SEARCH = deepcopy(BASE_10_SPARSITY_SEARCH)
+BASE_50_SPARSITY_SEARCH.update(
+    dataset_args=dict(
+        num_tasks=50,        # NUM_TASKS
+        root=os.path.expanduser("~/nta/results/data/"),
+        dim_context=1024,
+        seed=42,
+        download=False,  # Change to True if running for the first time
+    ),
+
+    model_args=dict(
+        input_size=784,
+        output_size=10,
+        hidden_sizes=[2048, 2048],
+        num_segments=50,    # NUM_TASKS
+        dim_context=1024,
+        kw=True,
+        kw_percent_on=tune.sample_from(
+            lambda spec: np.random.choice([0.05, 0.1, 0.2, 0.0])),
+        dendrite_weight_sparsity=0.0,
+        weight_sparsity=tune.sample_from(
+            lambda spec: np.random.choice([0.90, 0.8, 0.5, 0.0])),
+    ),
+
+    tasks_to_validate=(0, 1, 40, 49, 50),  # Tasks on which to run validate
+    epochs=1,
+    num_samples=8,
+    num_tasks=50,  # NUM_TASKS
+    num_classes=500,  # 10 * NUM_TASKS
+
+    optimizer_class=torch.optim.Adam,
+    optimizer_args=dict(
+        lr=tune.sample_from(lambda spec: np.random.choice([0.001, 0.0005])),
+    ),
+)
+
 # Export configurations in this file
 CONFIGS = dict(
     default_base=DEFAULT_BASE,
+    base_10_tasks=BASE_10_TASKS,
+    base_10_sparsity=BASE_10_SPARSITY_SEARCH,
+    base_50_search=BASE_50_SPARSITY_SEARCH,
     base2=BASE2,
 )
