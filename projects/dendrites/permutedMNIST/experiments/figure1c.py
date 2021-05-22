c26282f34f08c8cb388d72fad5371ee717bb2619
jeremy forest <jerem.forest@gmail.com>
2021-05-22 11:09:27 -0400

    updated legend on text size

--- projects/dendrites/permutedMNIST/experiments/figure1c.py
+++ projects/dendrites/permutedMNIST/experiments/figure1c.py
@@ -43,9 +43,9 @@ def cns_figure_1c():
     ort = "v"
     pal = "Set2"
     sigma = .2
-    fig.suptitle('Impact of the number of dendritic segments or the \n \
+    fig.suptitle('Impact of the number of dendritic segments or the\n \
                  activation sparsity on 10-tasks permuted MNIST performance',
-                 fontsize=12)
+                 fontsize=16)
 
     pt.RainCloud(x = x1, y = y, data = df1, palette = pal, bw = sigma,
                      width_viol = .6, ax = ax1, orient = ort, move = .3,
@@ -54,12 +54,12 @@ def cns_figure_1c():
                      width_viol = .6, ax = ax2, orient = ort, move = .3,
                      pointplot = True, alpha = .65)
     ax1.set_ylim([.68, .98])
-    ax1.set_ylabel('Mean accuracy')
-    ax1.set_xlabel('Number of dendritic segments (n)')
+    ax1.set_ylabel('Mean accuracy', fontsize=16)
+    ax1.set_xlabel('Number of dendritic segments (n)', fontsize=16)
     ax2.set_ylim([.68, .98])
     ax2.set_ylabel('')
-    ax2.set(yticklabels=[])
-    ax2.set_xlabel('Activation sparsity (k-winner %)')
+    ax2.set_xticklabels(['0.99', '0.95', '0.9', '0.8', '0.6', '0.4', '0.2', '0.1'])
+    ax2.set_xlabel('Activation sparsity', fontsize=16)
 
     if savefigs:
         plt.savefig('cns2021_figure1c.png', bbox_inches='tight',
