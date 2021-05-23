1928417d92fccc5447249f72769d916b4b3f9a5a
Alexander Cuozzo <acuozzo@numenta.com>
2021-05-23 09:17:13 -0500

    debugging changes

--- projects/greedy_infomax/debugging.py
+++ projects/greedy_infomax/debugging.py
@@ -30,60 +30,10 @@ from nupic.research.frameworks.greedy_infomax.utils.loss_utils import (
     multiple_cross_entropy,
     multiple_log_softmax_nll_loss,
 )
-from nupic.research.frameworks.greedy_infomax.models import FullVisionModel
+from nupic.research.frameworks.greedy_infomax.utils.data_utils import STL10_DATASET_ARGS
+from nupic.research.frameworks.greedy_infomax.models import SparseFullVisionModel, \
+    ClassificationModel
 from nupic.research.frameworks.vernon import experiments, mixins
-
-
-# get transforms for the dataset
-def get_transforms(val=False, aug=None):
-    trans = []
-
-    if aug["randcrop"]:
-        if val:
-            trans.append(transforms.RandomCrop(aug["randcrop"]))
-        else:
-            trans.append(transforms.CenterCrop(aug["randcrop"]))
-
-    if aug["flip"] and not val:
-        trans.append(transforms.RandomHorizontalFlip())
-
-    if aug["grayscale"]:
-        trans.append(transforms.Grayscale())
-        trans.append(transforms.ToTensor())
-        trans.append(transforms.Normalize(mean=aug["bw_mean"], std=aug["bw_std"]))
-    elif aug["mean"]:
-        trans.append(transforms.ToTensor())
-        trans.append(transforms.Normalize(mean=aug["mean"], std=aug["std"]))
-    else:
-        trans.append(transforms.ToTensor())
-
-    trans = transforms.Compose(trans)
-    return trans
-
-
-# labeled train set: mean [0.4469, 0.4400, 0.4069], std [0.2603, 0.2566, 0.2713]
-aug = {
-    "randcrop": 64,
-    "flip": True,
-    "grayscale": True,
-    "mean": [0.4313, 0.4156, 0.3663],
-    "std": [0.2683, 0.2610, 0.2687],
-    "bw_mean": [0.4120],
-    "bw_std": [0.2570],
-}
-transform_unsupervised = transform_supervised = get_transforms(val=False, aug=aug)
-transform_validation = trans = get_transforms(val=True, aug=aug)
-
-base_dataset_args = dict(root="~/nta/data/STL10", download=False)
-
-unsupervised_dataset_args = deepcopy(base_dataset_args)
-unsupervised_dataset_args.update(
-    dict(transform=transform_unsupervised, split="unlabeled")
-)
-supervised_dataset_args = deepcopy(base_dataset_args)
-supervised_dataset_args.update(dict(transform=transform_supervised, split="train"))
-validation_dataset_args = deepcopy(base_dataset_args)
-validation_dataset_args.update(dict(transform=transform_validation, split="test"))
 
 BATCH_SIZE = 32
 NUM_CLASSES = 10
@@ -95,11 +45,7 @@ self_supervised_config = dict(
                     name="paper-replication-small-baseline-2"),
     # Dataset
     dataset_class=STL10,
-    dataset_args=dict(
-        unsupervised=unsupervised_dataset_args,
-        supervised=supervised_dataset_args,
-        validation=validation_dataset_args,
-    ),
+    dataset_args=STL10_DATASET_ARGS,
     #               STL10:
     # 500 training images (10 pre-defined folds)
     # 8000 test images (800 test images per class)
@@ -113,25 +59,25 @@ self_supervised_config = dict(
     # Number of times to sample from the hyperparameter space. If `grid_search` is
     # provided the grid will be repeated `num_samples` of times.
     # Training batch size
-    batch_size=1,
+    batch_size=4,
     # Supervised batch size
-    batch_size_supervised=32,
+    batch_size_supervised=3,
     # Validation batch size
-    val_batch_size=32,
+    val_batch_size=5,
     # Number of batches per epoch. Useful for debugging
     batches_in_epoch=2,
-    # batches_in_epoch_supervised=1,
-    # batches_in_epoch_val=1,
+    batches_in_epoch_supervised=2,
+    batches_in_epoch_val=2,
     # Update this to stop training when accuracy reaches the metric value
     # For example, stop=dict(mean_accuracy=0.75),
     stop=dict(),
     # Number of epochs
     epochs=NUM_EPOCHS,
-    epochs_to_validate=[],
+    epochs_to_validate=[0],
     # Which epochs to run and report inference over the validation dataset.
     # epochs_to_validate=range(-1, 30),  # defaults to the last 3 epochs
     # Model class. Must inherit from "torch.nn.Module"
-    model_class=FullVisionModel,
+    model_class=SparseFullVisionModel,
     # default model arguments
     model_args=dict(
         negative_samples=16,
@@ -140,22 +86,23 @@ self_supervised_config = dict(
         grayscale=True,
         patch_size=16,
         overlap=2,
+        sparsity=[0.5, 0.5, 0.5]
     ),
     classifier_config=dict(
-        model_class=torch.nn.Linear,
-        model_args=dict(in_features=256, out_features=NUM_CLASSES),
+        model_class=ClassificationModel,
+        model_args=dict(in_channels=256, num_classes=NUM_CLASSES),
         loss_function=torch.nn.functional.cross_entropy,
         # Classifier Optimizer class. Must inherit from "torch.optim.Optimizer"
         optimizer_class=torch.optim.Adam,
         # Optimizer class class arguments passed to the constructor
-        optimizer_args=dict(lr=2e-4),
+        optimizer_args=dict(lr=2e-3),
     ),
     supervised_training_epochs_per_validation=1,
     loss_function=multiple_log_softmax_nll_loss,  # each GIM layer has a cross-entropy
     # Optimizer class. Must inherit from "torch.optim.Optimizer"
     optimizer_class=torch.optim.Adam,
     # Optimizer class class arguments passed to the constructor
-    optimizer_args=dict(lr=2e-4),
+    optimizer_args=dict(lr=2e-3),
     # # Learning rate scheduler class. Must inherit from "_LRScheduler"
     # lr_scheduler_class=torch.optim.lr_scheduler.StepLR,
     # Distributed parameters
