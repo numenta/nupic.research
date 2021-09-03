3794ee41950eade72b3703dd22d4befbb0de65e2
jeremy forest <jerem.forest@gmail.com>
2021-09-03 10:33:27 -0400

    HP 10 tasks with SI on dendrites with figure

--- projects/dendrites/permutedMNIST/hyperparameter_search/hyperparameters_dendrites_with_si_figures.py
+++ projects/dendrites/permutedMNIST/hyperparameter_search/hyperparameters_dendrites_with_si_figures.py
@@ -37,32 +37,10 @@ def hyperparameter_dendrites_wit_si_search_panel():
     sparsity and column 3 the weight sparsity.
     """
 
-    df_path1 = f"{experiment_folder}segment_search_with_si_lasttask.csv"
+    df_path1 = f"{experiment_folder}si_centroid_hp_10_lasttask.csv"
     df1 = pd.read_csv(df_path1)
 
-    df_path2 = f"{experiment_folder}kw_sparsity_search_with_si_lasttask.csv"
-    df2 = pd.read_csv(df_path2)
-
-    df_path3 = f"{experiment_folder}w_sparsity_search_with_si_lasttask.csv"
-    df3 = pd.read_csv(df_path3)
-
     df1 = df1[
-        [
-            "Activation sparsity",
-            "FF weight sparsity",
-            "Num segments",
-            "Accuracy",
-        ]
-    ]
-    df2 = df2[
-        [
-            "Activation sparsity",
-            "FF weight sparsity",
-            "Num segments",
-            "Accuracy",
-        ]
-    ]
-    df3 = df3[
         [
             "Activation sparsity",
             "FF weight sparsity",
@@ -73,23 +51,19 @@ def hyperparameter_dendrites_wit_si_search_panel():
 
     # Figure 1 'Impact of the different hyperparameters on performance
     # full cross product of hyperparameters
-    gs = gridspec.GridSpec(1, 3)
-    fig = plt.figure(figsize=(14, 5))
+    gs = gridspec.GridSpec(1, 1)
+    fig = plt.figure(figsize=(5, 5))
 
     ax1 = fig.add_subplot(gs[0, 0])
-    ax2 = fig.add_subplot(gs[0, 1])
-    ax3 = fig.add_subplot(gs[0, 2])
 
     x1 = "Num segments"
-    x2 = "Activation sparsity"
-    x3 = "FF weight sparsity"
 
     y = "Accuracy"
     ort = "v"
     pal = sns.color_palette(n_colors=6)
     sigma = 0.2
     fig.suptitle(
-        "Impact of the different hyperparameters on performance", fontsize=12
+        "Impact of the number of segments with SI on performance", fontsize=12
     )
 
     pt.RainCloud(
@@ -106,48 +80,14 @@ def hyperparameter_dendrites_wit_si_search_panel():
         alpha=0.65,
     )
 
-    pt.RainCloud(
-        x=x2,
-        y=y,
-        data=df2,
-        palette=pal,
-        bw=sigma,
-        width_viol=0.6,
-        ax=ax2,
-        orient=ort,
-        move=0.2,
-        pointplot=True,
-        alpha=0.65,
-    )
-
-    pt.RainCloud(
-        x=x3,
-        y=y,
-        data=df3,
-        palette=pal,
-        bw=sigma,
-        width_viol=0.6,
-        ax=ax3,
-        orient=ort,
-        move=0.2,
-        pointplot=True,
-        alpha=0.65,
-    )
-
     ax1.set_ylabel("Mean accuracy", fontsize=16)
     ax1.set_xlabel("Number of dendritic segments", fontsize=16)
-
-    ax2.set(ylabel="")
-    ax2.set_xlabel("Activation density", fontsize=16)
-
-    ax3.set(ylabel="")
-    ax3.set_xlabel("FF Weight density", fontsize=16)
 
     # Add 10 tasks label
     plt.figtext(-0.02, 0.7, "10 TASKS", fontsize=16)
 
     fig.suptitle(
-        """Impact of different hyperparameters on \n 10-tasks
+        """Impact of the number of segments on \n 10-tasks
         permuted MNIST performance""",
         fontsize=16,
     )
