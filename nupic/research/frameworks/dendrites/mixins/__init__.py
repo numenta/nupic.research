72ccd6ef29bc8cfb53721baa3d277186423c90ef
Ben Cohen <benjapaulcohen@gmail.com>
2021-10-21 14:31:31 -0700

    remove drafted concat one hot context

--- nupic/research/frameworks/dendrites/mixins/__init__.py
+++ nupic/research/frameworks/dendrites/mixins/__init__.py
@@ -20,7 +20,6 @@
 # ----------------------------------------------------------------------
 
 from .centroid_figure_1b import *
-from .concat_one_hot_context import *
 from .eval_per_task import *
 from .sp_context_analysis import *
 from .sp_context import SpatialPoolerContext
