8a6da9f9c37de6f9b3fb8dd53b349bfe110c53bf
Alexander Cuozzo <acuozzo@numenta.com>
2021-11-02 16:20:05 -0700

    more refactor functionality

--- nupic/research/frameworks/greedy_infomax/models/gim_model.py
+++ nupic/research/frameworks/greedy_infomax/models/gim_model.py
@@ -27,15 +27,16 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from .gim_block import GreedyInfoMaxBlock
-
+from .gim_block import GreedyInfoMaxBlock, InfoEstimateAggregator, EncodingAggregator
+from .utility_layers import PatchifyInputs
 class GreedyInfoMaxModel(nn.Sequential):
     """
     A model which wraps another model and adds Greedy InfoMax functionality. It does
-    this by adding a PatchifyInputs layer to the beginning of the network, and also
-    tracks the info estimates/encodings with the InfoEstimateAggregator and
-    EncodingAggregator respectively.
-
+    this by
+    1. adding a PatchifyInputs layer to the beginning of the network
+    2. adds GIM blocks to the network and connects them using forward hooks on the model
+    2. tracks the info estimates using InfoEstimateAggregator
+    3. tracks the encodings using the EncodingAggregator
     """
     def __init__(self,
                  model,
@@ -45,9 +46,43 @@ class GreedyInfoMaxModel(nn.Sequential):
                  patch_size=5,
                  overlap=2):
         pass
+        self.training_mode = "unsupervised"
+        self.model = model
+        self.gim_blocks = []
+        self.info_estimate_aggregator = InfoEstimateAggregator(k_predictions,
+                                                               negative_samples)
+        self.encoding_aggregator = EncodingAggregator()
+        self.create_gim_blocks(named_modules, model) # TODO: create this method
+
+        self.patchify_inputs = PatchifyInputs(patch_size, overlap)
 
     def set_training_mode(self, training_mode="unsupervised"):
+        """
+        Sets the training mode for the model.
+        """
         if training_mode not in ["unsupervised", "supervised"]:
             raise Exception("Training mode must be either 'unsupervised' or 'supervised'")
+        self.training_mode = training_mode
+
+        # go through each of the registered modules and set its hook to its
+        # corresponding GreedyInfoMaxBlock to the correct mode. For example,
+        # in unsupervised training, go through each of the modules and set the hook
+        # to the forward_unsupervised function of the GreedyInfoMaxBlock.
+        for name, module in self.named_modules():
+            if self.training_mode == "unsupervised":
+                module.set_forward_hook(module.forward_unsupervised)
+
+
+    def forward_unsupervised(self, x):
+        """
+        Forward pass for unsupervised training.
+        """
+        self.set_mode("unsupervised")
+        return self.forward(x)
+
+    def forward_supervised(self, x):
+        """
+        Forward pass for supervised training.
+        """
         pass
 
