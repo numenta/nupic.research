37874b5f69da9506a5bc39f690bc6c536ccd525d
Lucas <lucasosouza@gmail.com>
2021-12-22 14:18:25 -0800

    Initial setup of DQN, with a run file

---
+++ projects/dqn/src/__init__.py
@@ -0,0 +1,26 @@
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2022, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
+from .rl_experiment import OffPolicyReinforcementLearningExperiment
+from .networks import DQNNetwork
+from .misc import Scheduler
+from .algorithms import DQN
+from .data_utils import Experience, EnvDataset, InteractiveDataLoader, ReplayBuffer
